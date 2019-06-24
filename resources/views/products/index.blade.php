0156d1858e04d395fac87fa067fb7b62db540cf6
Dope <yjuyjuy@gmail.com>
2019-06-23 23:16:29 -0500

    added class to hide filters and sort on xs-md devices

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
 	<form action="{{ url()->current() }}" class="row justify-content-center">
-		<div class="col-auto d-flex">
+		<div class="col-auto d-none d-md-flex">
 			<div class="mx-auto">
 				@include('products.index.filters')
 			</div>
@@ -11,7 +11,7 @@
 		<div class="col">
 				@include('products.index.products')
 		</div>
-		<div class="col-auto d-flex">
+		<div class="col-auto d-none d-md-flex">
 			<div class="mx-auto">
 				@include('products.index.sort')
 			</div>
