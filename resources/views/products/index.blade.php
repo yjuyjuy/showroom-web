3bcdaf4433a665fe7940f0d858f257c6148bf46c
Dope <yjuyjuy@gmail.com>
2019-07-14 11:41:04 -0500

    change naming added link to show product

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,30 +1,32 @@
 @extends('layouts.app')
 @section('title','所有商品 - TheShowroom')
 @section('content')
-<div class="pt-2">
-	<ul class="mdc-image-list my-image-list">
+<div id="products-index" class="">
+	<ul class="mdc-image-list main-image-list">
 		@foreach($products as $product)
 		<li class="mdc-image-list__item">
-			<div class="">
-				<img class="mdc-image-list__image" src="{{$product->images->first()->url ?? asset('storage/icons/ImagePlaceholder.svg')}}">
-			</div>
-			<div class="mdc-image-list__supporting">
-				<span class="mdc-image-list__label brand">{{ $product->brand->name }}</span>
-				<span class="mdc-image-list__label">{{ $product->name_cn }}</span>
-				@if($product->price)
-				<span class="mdc-image-list__label">
-					{{ "\u{00a5}".$product->price }}
-				</span>
-				@else
-				<span class="mdc-image-list__label">
-					缺货
-				</span>
-				@endif
-			</div>
+			<a href="{{ route('products.show',['product' => $product ]) }}">
+				<div class="">
+					<img class="mdc-image-list__image" src="{{$product->images->first()->url ?? asset('storage/icons/ImagePlaceholder.svg')}}">
+				</div>
+				<div class="mdc-image-list__supporting">
+					<span class="mdc-image-list__label brand">{{ $product->brand->name }}</span>
+					<span class="mdc-image-list__label">{{ $product->name_cn }}</span>
+					@if($product->price)
+					<span class="mdc-image-list__label">
+						{{ "\u{00a5}".$product->price }}
+					</span>
+					@else
+					<span class="mdc-image-list__label">
+						缺货
+					</span>
+					@endif
+				</div>
+			</a>
 		</li>
 		@endforeach
 	</ul>
-	<button id="open-options-fab" class="mdc-fab" aria-label="Favorite">
+	<button id="display-options-fab" class="mdc-fab" aria-label="Favorite">
 	  <span class="mdc-fab__icon material-icons">favorite</span>
 	</button>
 	<div id="display-options-dialog"
@@ -47,10 +49,10 @@
 	      </div>
 
 	      <footer class="mdc-dialog__actions">
-	        <button id="mdc-dialog__cancel-button" type="button" class="mdc-button mdc-dialog__button" data-mdc-dialog-action="close">
+	        <button type="button" class="mdc-button mdc-button--error mdc-dialog__button" data-mdc-dialog-action="close">
 	          <span class="mdc-button__label">Cancel</span>
 	        </button>
-	        <button id="mdc-dialog__accept-button" type="button" class="mdc-button mdc-button--unelevated mdc-dialog__button" data-mdc-dialog-action="accept">
+	        <button type="button" class="mdc-button mdc-button--unelevated mdc-dialog__button" data-mdc-dialog-action="accept">
 	          <span class="mdc-button__label">OK</span>
 	        </button>
 	      </footer>
