0f77a89e2bf052bb63c614256cf735c4bf53301f
Dope <yjuyjuy@gmail.com>
2019-07-15 23:21:09 -0500

    finished prices.index view

--- resources/views/prices/index.blade.php
+++ resources/views/prices/index.blade.php
@@ -3,63 +3,63 @@
 @section('title','价格表 - '.$vendor->name.' - TheShowroom')
 
 @section('content')
-<div class="container">
+<div class="d-flex flex-column align-items-center">
 	@if(auth()->user()->isSuperAdmin())
-	<div class="row mb-4">
-		<form method="get" target="_self" class="col-auto mx-auto">
-			<select onchange="submit()" class="form-control" name="vendor">
-				<option value=""></option>
-				@foreach(\App\Vendor::all() as $v)
-				<option value="{{$v->id}}" {{(old('vendor') == $v->id)?' selected':''}}>{{$v->name}}</option>
-				@endforeach
-			</select>
-		</form>
-	</div>
+		<div class="mdc-select mb-4 optional-form-field">
+		  <input type="hidden" name="vendor" onchange="document.getElementById('change-vendor-form').submit()" form="change-vendor-form">
+		  <i class="mdc-select__dropdown-icon"></i>
+		  <div class="mdc-select__selected-text"></div>
+		  <div class="mdc-select__menu mdc-menu mdc-menu-surface">
+		    <ul class="mdc-list">
+					@foreach(\App\Vendor::all() as $v)
+		      <li class="mdc-list-item" data-value="{{ $v->id }}">{{ $v->name }}</li>
+		      @endforeach
+		    </ul>
+		  </div>
+		  <span class="mdc-floating-label">{{ __('vendor') }}</span>
+		  <div class="mdc-line-ripple"></div>
+		</div>
+		<form id="change-vendor-form" method="get" target="_self" class="d-none"></form>
 	@endif
+
 	@foreach($vendor->products as $product)
-	<div class="row mb-md-4">
-		<div class="col-md-6">
-			<div class="row">
-				<div class="col-6">
-					<img class="w-100" src="{{$product->images[0]->url ?? ''}}">
+	<div class="images-content-container mb-4">
+		<div class="images-container">
+			<div class="d-flex">
+				<div class="mx-2">
+					<img src="{{$product->images[0]->url ?? ''}}">
 				</div>
-				<div class="col-6">
-					<img class="w-100" src="{{$product->images[1]->url ?? ''}}">
+				<div class="mx-2">
+					<img src="{{$product->images[1]->url ?? ''}}">
 				</div>
 			</div>
 		</div>
-		<div class="col-md-6 d-flex justify-content-center align-items-center">
-			<div class="" style="min-width:83.3%;">
-				<div class="d-flex flex-column p-4">
-					<div class="row pb-2 mb-2 border-bottom">
-						<div class="col-12">
-							<span>{{ $product->brand->full_name }}</span>
-						</div>
-						<div class="col-12">
-							<a href="{{route('products.show',['product' => $product])}}" class="font-weight-bold">{{ $product->name_cn }}</a>
-						</div>
-					</div>
-					<div class="row">
-						<span class="col">尺码</span>
-						<span class="col">成本</span>
-						<span class="col">调货</span>
-						<span class="col">零售</span>
-					</div>
-					@foreach($product->prices as $price)
-					@foreach($price->data as $row)
-					<div class="row">
-						<span class="col">{{ $row['size'] }}</span>
-						<span class="col">&yen;{{$row['cost']}}</span>
-						<span class="col">&yen;{{$row['resell']}}</span>
-						<span class="col">&yen;{{$row['retail']}}</span>
-					</div>
-					@endforeach
-					<div class="row justify-content-end">
-						<a href="{{route('prices.edit',['price'=>$price])}}" class="col-auto text-primary text-right pr-0">修改</a>
-						<a href="#" class="col-auto col-md-2 text-danger" @click.prevent="deletePrice({{$price->id}})">删除</a>
-					</div>
-					@endforeach
+		<div class="content-container d-flex flex-column justify-content-center align-items-center text-right">
+			<div class="price-grid p-4">
+				<div class="price-grid__header d-flex flex-column">
+					<span>{{ $product->brand->full_name }}</span>
+					<a href="{{route('products.show',['product' => $product])}}" class="font-weight-bold">{{ $product->name_cn }}</a>
 				</div>
+				<div class="price-grid__row">
+					<span class="price-grid__col">尺码</span>
+					<span class="price-grid__col">成本</span>
+					<span class="price-grid__col">调货</span>
+					<span class="price-grid__col">零售</span>
+				</div>
+				@foreach($product->prices as $price)
+					@foreach($price->data as $row)
+						<div class="price-grid__row">
+							<span class="price-grid__col">{{ $row['size'] }}</span>
+							<span class="price-grid__col">&yen;{{$row['cost']}}</span>
+							<span class="price-grid__col">&yen;{{$row['resell']}}</span>
+							<span class="price-grid__col">&yen;{{$row['retail']}}</span>
+						</div>
+					@endforeach
+					<div class="price-grid__footer d-flex justify-content-end">
+						<a href="{{route('prices.edit',['price'=>$price])}}" class="mdc-button">修改</a>
+						<a href="#" class="mdc-button mdc-button--error" @click.prevent="deletePrice({{$price->id}})">删除</a>
+					</div>
+				@endforeach
 			</div>
 		</div>
 	</div>
