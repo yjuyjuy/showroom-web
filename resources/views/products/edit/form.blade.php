6ad819737a4cc285068f38a687e503c6453995b6
Dope <yjuyjuy@gmail.com>
2019-07-15 23:17:03 -0500

    fix create/edit product form

--- resources/views/products/edit/form.blade.php
+++ resources/views/products/edit/form.blade.php
@@ -5,7 +5,7 @@
 	<div class="mdc-select__menu mdc-menu mdc-menu-surface">
 		<ul class="mdc-list">
 			@foreach(\App\Brand::all() as $brand)
-			<?php $selected = old('brand', optional($product)->brand->id) === $brand->id; ?>
+			<?php $selected = old('brand') ?? $product->brand->id ?? null === $brand->id; ?>
 			@if($selected)
 			<li data-value="{{ $brand->id }}" class="mdc-list-item mdc-list-item--selected" aria-selected="true">
 				@else
@@ -31,7 +31,7 @@
 	<div class="mdc-select__menu mdc-menu mdc-menu-surface">
 		<ul class="mdc-list">
 			@foreach(\App\Season::all() as $season)
-			<?php $selected = old('season', optional($product)->season->id) === $season->id; ?>
+			<?php $selected = old('season') ?? $product->season->id ?? null === $season->id; ?>
 			@if($selected)
 			<li data-value="{{ $season->id }}" class="mdc-list-item mdc-list-item--selected" aria-selected="true">
 				@else
@@ -50,7 +50,7 @@
 @enderror
 
 <div class="mdc-text-field">
-	<input type="text" class="mdc-text-field__input" name="name_cn" autocomplete="off" value="{{ old('name_cn') ?? ( $product->name_cn ?? '' ) }}">
+	<input type="text" class="mdc-text-field__input" name="name_cn" autocomplete="off" value="{{ old('name_cn') ?? $product->name_cn ?? '' }}">
 	<label class="mdc-floating-label" for="my-text-field">{{ __('product name_cn') }}</label>
 	<div class="mdc-line-ripple"></div>
 </div>
@@ -62,7 +62,7 @@
 @enderror
 
 <div class="mdc-text-field">
-	<input type="text" class="mdc-text-field__input" name="name" autocomplete="off" value="{{ old('name') ?? ( $product->name ?? '' ) }}">
+	<input type="text" class="mdc-text-field__input" name="name" autocomplete="off" value="{{ old('name') ?? $product->name ?? '' }}">
 	<label class="mdc-floating-label" for="my-text-field">{{ __('product name') }}</label>
 	<div class="mdc-line-ripple"></div>
 </div>
@@ -79,7 +79,7 @@
 	<div class="mdc-select__menu mdc-menu mdc-menu-surface">
 		<ul class="mdc-list">
 			@foreach(\App\Category::all() as $category)
-			<?php $selected = old('category', optional($product)->category->id) === $category->id; ?>
+			<?php $selected = old('category') ?? $product->category->id ?? null === $category->id; ?>
 			@if($selected)
 			<li data-value="{{ $category->id }}" class="mdc-list-item mdc-list-item--selected" aria-selected="true">
 				@else
@@ -104,7 +104,7 @@
 	<div class="mdc-select__menu mdc-menu mdc-menu-surface">
 		<ul class="mdc-list">
 			@foreach(\App\Color::all() as $color)
-			<?php $selected = old('color', optional($product)->color->id) === $color->id; ?>
+			<?php $selected = old('color') ?? $product->color->id ?? null === $color->id; ?>
 			@if($selected)
 			<li data-value="{{ $color->id }}" class="mdc-list-item mdc-list-item--selected" aria-selected="true">
 				@else
@@ -122,16 +122,16 @@
 <p class="mdc-select-helper-text mdc-select-helper-text--persistent" role="alert">{{ $message }}</p>
 @enderror
 
-<div class="mdc-text-field mdc-text-field--textarea optional-form-field">
-	<textarea class="mdc-text-field__input" name="comment" rows="2">{{ old('comment') ?? ( $product->comment ?? '' ) }}</textarea>
+<label class="mdc-text-field mdc-text-field--textarea optional-form-field">
+	<textarea class="mdc-text-field__input" name="comment" rows="2">{{ old('comment') ?? $product->comment ?? '' }}</textarea>
 	<div class="mdc-notched-outline">
 		<div class="mdc-notched-outline__leading"></div>
 		<div class="mdc-notched-outline__notch">
-			<label for="textarea" class="mdc-floating-label">{{ __('comment') }}</label>
+			<span class="mdc-floating-label">{{ __('comment') }}</span>
 		</div>
 		<div class="mdc-notched-outline__trailing"></div>
 	</div>
-</div>
+</label>
 @error('comment')
 <div class="mdc-text-field-helper-line">
 	<div class="mdc-text-field-helper-text mdc-text-field-helper-text--persistent">{{ $message }}</div>
