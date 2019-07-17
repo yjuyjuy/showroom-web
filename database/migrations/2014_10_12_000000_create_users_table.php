872a719d9cfab41fb0414d0903ccf9e82660771b
Dope <yjuyjuy@gmail.com>
2019-07-17 18:07:57 -0500

    changed user and vendor model to allow vendors to have multiple users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,11 @@ class CreateUsersTable extends Migration
 			$table->string('email')->unique();
 			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password');
+			$table->unsignedBigInteger('vendor_id')->nullable();
 			$table->rememberToken();
 			$table->timestamps();
+
+			$table->index('vendor_id');
 		});
 	}
 
