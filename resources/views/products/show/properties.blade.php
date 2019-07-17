d0791033f782e2fe29106b98f8156b16f7c60226
Dope <yjuyjuy@gmail.com>
2019-07-16 21:43:06 -0500

    fix clickbox size

--- resources/views/products/show/properties.blade.php
+++ resources/views/products/show/properties.blade.php
@@ -1,11 +1,15 @@
 <div class="d-flex flex-column products-show__info--properties">
-	<a id="product-brand" href="{{ route('products.index',['brand' => [$product->brand->id]]) }}">
-		{{ $product->brand->full_name }}</a>
+	<div class="">
+		<a id="product-brand" href="{{ route('products.index',['brand' => [$product->brand->id]]) }}">
+			{{ $product->brand->full_name }}</a>
+	</div>
 	<div>
 		<a id="product-season" href="{{ route('products.index',['season' => [$product->season->id]]) }}">{{ $product->season->name }}</a>
 		<span id="product-name">{{ $product->name_cn }}</span>
 	</div>
 	@can('update',$product)
-	<a href="{{ route('products.edit',['product' => $product ]) }}">Edit</a>
+	<div class="">
+		<a href="{{ route('products.edit',['product' => $product ]) }}">Edit</a>
+	</div>
 	@endcan
 </div>
