a6cfe7bf3c3ed09bec693d85cd7c05f7cf7737df
Dope <yjuyjuy@gmail.com>
2019-07-16 21:43:32 -0500

    use image-content-container class in products.create view

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -3,23 +3,19 @@
 @section('title','创建新商品 - TheShowroom')
 
 @section('content')
-<div id="products.create">
-	<div class="row">
-
-		<div class="col-md-6 mx-auto">
-			<form id="create-product-form" class="product-form" action="{{route('products.store')}}" method="post">
-				@csrf
-				@include('products.edit.form')
-				<div class="form-group">
-					<div class="d-flex justify-content-end mt-3">
-						<button class="mdc-button mdc-button--outlined">
-						  <span class="mdc-button__label">{{ __('create') }}</span>
-						</button>
-					</div>
+<div class="images-content-container">
+	<div class="content-container">
+		<form id="create-product-form" class="product-form" action="{{route('products.store')}}" method="post">
+			@csrf
+			@include('products.edit.form')
+			<div class="form-group">
+				<div class="d-flex justify-content-end mt-3">
+					<button class="mdc-button mdc-button--outlined">
+					  <span class="mdc-button__label">{{ __('create') }}</span>
+					</button>
 				</div>
-			</form>
-		</div>
-
+			</div>
+		</form>
 	</div>
 </div>
 @endsection
