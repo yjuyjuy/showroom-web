10e74daa69d07ef37cd8fc5cfddd50e5775d872e
Dope <yjuyjuy@gmail.com>
2019-07-17 23:52:11 -0500

    in display option dialog added clear button, changed cancel button to close, added translation

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -53,7 +53,11 @@
 
 	      <footer class="mdc-dialog__actions">
 	        <button type="button" class="mdc-button mdc-button--error mdc-dialog__button" data-mdc-dialog-action="close">
-	          <span class="mdc-button__label">{{ __('cancel') }}</span>
+	          <span class="mdc-button__label">{{ __('close') }}</span>
+	        </button>
+					<button type="button" class="mdc-button mdc-dialog__button"
+									onclick="[].map.call(document.getElementById('display-option-form').elements,function(el) { el.checked = false; el.selected = false;});">
+	          <span class="mdc-button__label">{{ __('clear') }}</span>
 	        </button>
 	        <button type="submit" class="mdc-button mdc-button--unelevated mdc-dialog__button" data-mdc-dialog-action="accept" form="display-option-form">
 	          <span class="mdc-button__label">{{ __('apply') }}</span>
