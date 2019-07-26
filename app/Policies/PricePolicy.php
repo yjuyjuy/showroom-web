64d3210bc19887af94cee956fce323ed8c1345dc
Dope <yjuyjuy@gmail.com>
2019-07-26 12:49:24 -0500

    refactored PricePolicy

--- app/Policies/PricePolicy.php
+++ app/Policies/PricePolicy.php
@@ -8,16 +8,87 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 
 class PricePolicy
 {
-	use HandlesAuthorization;
-	/**
-	 * Determine whether the user can delete the price.
-	 *
-	 * @param  \App\User  $user
-	 * @param  \App\Price  $price
-	 * @return mixed
-	 */
-	public function delete(User $user, Price $price)
-	{
-		return $user->isSuperAdmin() || ($user->id == $price->vendor->user_id);
-	}
+		use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any prices.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the price.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Price  $price
+     * @return mixed
+     */
+    public function view(User $user, Price $price)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create prices.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+			return $user->isSuperAdmin() || $user->vendor;
+    }
+
+    /**
+     * Determine whether the user can update the price.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Price  $price
+     * @return mixed
+     */
+    public function update(User $user, Price $price)
+    {
+			return $user->isSuperAdmin() || ($user->id == $price->vendor->user_id);
+    }
+
+    /**
+     * Determine whether the user can delete the price.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Price  $price
+     * @return mixed
+     */
+    public function delete(User $user, Price $price)
+    {
+			return $user->isSuperAdmin() || ($user->id == $price->vendor->user_id);
+    }
+
+    /**
+     * Determine whether the user can restore the price.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Price  $price
+     * @return mixed
+     */
+    public function restore(User $user, Price $price)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the price.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Price  $price
+     * @return mixed
+     */
+    public function forceDelete(User $user, Price $price)
+    {
+      return false;
+    }
 }
