07a7214de948f54e41a7c47e4a0bd53170f1cd44
Dope <yjuyjuy@gmail.com>
2019-07-31 22:17:46 -0500

    dropped seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,25 +11,6 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run()
 	{
-		$user = new \App\User([
-					'id' => 1,
-					'email' => 'yjuyjuy@gmail.com',
-					'password' => Hash::make('123456789'),
-					'username' => 'Dope',
-		]);
-		$user->save();
-		$this->call([
-			BrandsTableSeeder::class,
-			CategoriesTableSeeder::class,
-			ColorsTableSeeder::class,
-			SeasonsTableSeeder::class,
-			VendorsTableSeeder::class,
-			WebsitesTableSeeder::class,
-			TypesTableSeeder::class,
-			ProductsTableSeeder::class,
-			PricesTableSeeder::class,
-		]);
-		$vendor = \App\Vendor::find(1);
-		$user->vendor()->associate($vendor)->save();
+		//
 	}
 }
