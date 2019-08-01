77d450f96fb7f91dda89c360f808c1e7cc3a7a58
Dope <yjuyjuy@gmail.com>
2019-07-31 21:27:10 -0500

    fixed bug in logs.index view caused by forceDeleted product

--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -3,12 +3,14 @@
 @section('content')
 	<div id="logs-index" class="d-flex flex-column px-2">
 		@foreach($logs as $log)
-		<div class="log-item">
-			<a href="{{route('products.show',['product' => $log->product])}}" class="log-content"><span class="timestamp" data-timestamp="{{ $log->created_at->valueOf() }}"></span>&nbsp;{{ $log->toString() }}</a>
-			<button class="mdc-icon-button material-icons log-delete-button"
-							data-href="{{ route('logs.destroy',['log' => $log]) }}"
-							onclick="axios.delete(this.dataset.href).then(response => {this.parentElement.parentElement.removeChild(this.parentElement);});">clear</button>
-		</div>
+			@if($log->product)
+			<div class="log-item">
+				<a href="{{route('products.show',['product' => $log->product])}}" class="log-content"><span class="timestamp" data-timestamp="{{ $log->created_at->valueOf() }}"></span>&nbsp;{{ $log->toString() }}</a>
+				<button class="mdc-icon-button material-icons log-delete-button"
+								data-href="{{ route('logs.destroy',['log' => $log]) }}"
+								onclick="axios.delete(this.dataset.href).then(response => {this.parentElement.parentElement.removeChild(this.parentElement);});">clear</button>
+			</div>
+			@endif
 		@endforeach
 	</div>
 @endsection
