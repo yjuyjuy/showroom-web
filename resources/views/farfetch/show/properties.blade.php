faa0f5192823ec9134866f6e7518200252e24561
Dope <yjuyjuy@gmail.com>
2019-08-02 15:55:19 -0500

    changed font-size and added designerStyleId in farfetch.show

--- resources/views/farfetch/show/properties.blade.php
+++ resources/views/farfetch/show/properties.blade.php
@@ -1,16 +1,18 @@
 <div class="d-flex flex-column products-show__info--properties">
 	<div class="">
-		<a id="product-brand" href="{{ route('farfetch.index',['designer' => [$product->designer->id]]) }}">
+		<a id="product-brand" href="{{ route('farfetch.index',['designer' => [$product->designer->id]]) }}"
+			 class="mdc-typography--headline6">
 			{{ __($product->designer->name) }}</a>
 	</div>
-	<div>
+	<div class="mdc-typography--headline6" style="text-transform: capitalize;">
 		<span id="product-name">
 			{{ $product->shortDescription }}
 		</span>
-	</div>
-	<div>
 		<span id="product-color">
 			{{ __($product->colors) }}
 		</span>
+	</div>
+	<div class="mt-1">
+		<a href="#" onclick="event.preventDefault(); navigator.clipboard.writeText(this.textContent);">{{$product->designerStyleId}}</a>
 	</div>
 </div>
