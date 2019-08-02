47c2857f2944aae6807cb9565bdcab80fcb60603
Dope <yjuyjuy@gmail.com>
2019-08-02 15:42:42 -0500

    open images.edit view using window.location.replace() to improve navigation experience

--- resources/views/products/show/images.blade.php
+++ resources/views/products/show/images.blade.php
@@ -1,5 +1,8 @@
 
 <images-slider :images='@json($product->images->pluck('url')->toArray())'></images-slider>
 <div class="d-flex justify-content-center">
-	@can('update',\App\Image::class)<a href="{{route('images.edit',['product'=>$product])}}">管理图片</a>@endcan
+	@can('update',\App\Image::class)
+		<a href="{{route('images.edit',['product'=>$product])}}"
+			 onclick="event.preventDefault(); window.location.replace(this.href);">管理图片</a>
+	@endcan
 </div>
