13ad5e1405de02bdbbc0733e7410c8968d6498dc
Dope <yjuyjuy@gmail.com>
2019-08-03 19:41:08 -0500

    restyled products.show.properties view

--- resources/views/products/show/properties.blade.php
+++ resources/views/products/show/properties.blade.php
@@ -1,33 +1,24 @@
 <div class="d-flex flex-column products-show__info--properties">
-	<div class="">
-		<a id="product-brand" href="{{ route('products.index',['brand' => [$product->brand->id]]) }}"
-			 class="mdc-typography--headline6">
+
+	<div class="mdc-typography--headline6">
+		<a id="product-brand" href="{{ route('products.index',['brand' => [$product->brand->id]]) }}">
 			{{ __($product->brand->full_name) }}</a>
 	</div>
 	<div class="mdc-typography--headline6">
-		<a id="product-season" href="{{ route('products.index',['season' => [$product->season->id]]) }}">{{ __($product->season->name) }}</a>
-		<a id="product-name" href="{{ route('products.show',['product' => $product]) }}" onclick="event.preventDefault(); window.location.replace(this.href);">
+		<a class="product-name" href="{{ route('products.show',['product' => $product]) }}" onclick="event.preventDefault(); window.location.replace(this.href);">
 			{{ $product->name_cn }}
 		</a>
-		<span id="product-color">
-			{{ __($product->color->name) }}
-		</span>
 	</div>
-	<div class="my-2"></div>
-	<div>
-		<span id="product-name">
-			{{ $product->name }}
-		</span>
-	</div>
+
 	@can('update',$product)
-	@if($product->designerStyleId)
-	<div class="">
-		{{$product->designerStyleId}}
-	</div>
-	@endif
-	<div class="">
-		<span class="mr-2">{{ $product->id }}</span>
-		<a href="{{ route('products.edit',['product' => $product ]) }}" onclick="event.preventDefault(); window.location.replace(this.href);">{{ __('edit') }}</a>
+	<div class="mt-2">
+		<div style="text-transform:capitalize;">{{ $product->name }} {{ __($product->color->name) }}</div>
+		@if($product->designerStyleId)<div class="">{{ __('designerStyleId') }}: {{$product->designerStyleId}}</div>@endif
+		<div class="">
+			<span class="">ID: {{ $product->id }}</span>
+			<a href="{{ route('products.edit',['product' => $product ]) }}" onclick="event.preventDefault(); window.location.replace(this.href);">{{ __('edit') }}</a>
+		</div>
 	</div>
 	@endcan
+
 </div>
