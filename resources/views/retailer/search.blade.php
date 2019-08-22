4ff96441ab3834abb887274c9d979a48833dab00
Dope <yjuyjuy@gmail.com>
2019-08-21 21:06:15 -0500

    minor tweak on retailer.search view

--- resources/views/retailer/search.blade.php
+++ resources/views/retailer/search.blade.php
@@ -3,10 +3,10 @@
 
 @section('content')
 <form>
-	<div class="d-flex w-100 justify-content-center align-items-center">
+	<div class="d-flex w-100 justify-content-center align-items-center flex-wrap">
 		<div class="mdc-text-field mdc-text-field--outlined mdc-text-field--no-label">
 			<input type="text" name="name" class="mdc-text-field__input" aria-label="Label" autofocus
-			placeholder="@if($not_found){{ __('retailer not found') }}@else{{ __('Search retailer') }}@endif">
+			placeholder="{{ __('Search retailer') }}">
 			<div class="mdc-notched-outline">
 				<div class="mdc-notched-outline__leading"></div>
 				<div class="mdc-notched-outline__trailing"></div>
@@ -15,6 +15,12 @@
 		<button type="submit" class="mdc-icon-button material-icons ml-2">
 			<i class="material-icons">search</i>
 		</button>
+		@if($not_found)
+		<div class="w-100"></div>
+		<div class="mt-4">
+			<span>{{ __('retailer not found') }}"{{ old('name') }}"</span>
+		</div>
+		@endif
 	</div>
 </form>
 @endsection
