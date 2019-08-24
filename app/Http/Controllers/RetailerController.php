f2084e39b13198d005789906d824b361c8a70142
Dope <yjuyjuy@gmail.com>
2019-08-24 18:32:17 -0500

    sync changes in ProductController

--- app/Http/Controllers/RetailerController.php
+++ app/Http/Controllers/RetailerController.php
@@ -6,90 +6,61 @@ use App\Product;
 use App\Retailer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Validation\Rule;
 
 class RetailerController extends Controller
 {
-	public function search(Request $request)
-	{
-		$not_found = false;
-		if ($request->input('name')) {
-			$retailer = \App\Retailer::where('name', $request->input('name'))->first();
-			if ($retailer) {
-				return redirect(route('retailer.products.index', ['retailer' => $retailer,]));
-			} else {
-				$not_found = true;
-			}
-		}
-		return view('retailer.search', compact('not_found'));
-	}
-  
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
 	public function index(Request $request, Retailer $retailer)
 	{
-		$products = Cache::remember(url()->full(), 10, function () use ($retailer) {
-			$products = $this->filter($retailer->products());
-			$products = $this->sort($products);
-			$products->load(['image','brand','retails' => function ($query) use ($retailer) {
-				$query->where('retailer_id', $retailer->id);
-			}]);
-			return $products;
-		});
-		if ($request->input('sort') === 'random') {
-			$products = $products->shuffle();
-		}
 		$user = auth()->user();
-		if ($user) {
-			$products->each(function ($product, $index) use ($user) {
-				$product->retails = $product->retails->whereIn('retailer_id', $user->following_retailers->pluck('id'));
-			});
-		} else {
-			$products->each(function ($product, $index) use ($user) {
-				$product->retails = $product->retails->where('retailer_id', 1);
-			});
-		}
-
+		$products = $this->filter($retailer);
+		$products = $this->sort($products);
 		$sortOptions = $this->sortOptions();
 		$filters = $this->filterOptions();
 		$request->flash();
-		return view('retailer.products.index', compact('products', 'sortOptions', 'filters', 'user', 'retailer'));
+		return view('retailers.products.index', compact('products', 'sortOptions', 'filters', 'user', 'retailer'));
 	}
-  
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  \App\Product  $product
-	 * @return \Illuminate\Http\Response
-	 */
+
 	public function show(Retailer $retailer, Product $product)
 	{
 		$product->load(['images', 'retails' => function ($query) use ($retailer) {
 			$query->where('retailer_id', $retailer->id);
 		}]);
-		return view('retailer.products.show', compact('product', 'retailer'));
+		return view('retailers.products.show', compact('product', 'retailer'));
 	}
+
 	public function follow(Retailer $retailer)
 	{
 		$user = auth()->user();
 		return $user->following_retailers()->syncWithoutDetaching($retailer);
 	}
-	
+
   public function unfollow(Retailer $retailer)
 	{
 		$user = auth()->user();
 		return $user->following_retailers()->detach($retailer);
 	}
-	
-  public function following()
+
+  public function following(Request $request)
 	{
+		$not_found = false;
+		if ($request->input('name')) {
+			$name = $request->validate([
+				'name' => ['sometimes', 'string', 'max:255'],
+			])['name'];
+			$retailer = \App\Retailer::where('name', $name)->first();
+			if ($retailer) {
+				return redirect(route('retailer.products.index', ['retailer' => $retailer,]));
+			} else {
+				$not_found = true;
+			}
+			$request->flash();
+		}
 		$user = auth()->user();
 		$retailers = $user->following_retailers;
-		return view('retailer.following', compact('retailers'));
+		return view('retailers.following', compact('retailers', 'not_found'));
 	}
-	
+
   public function validateFilters()
 	{
 		return request()->validate([
@@ -99,57 +70,31 @@ class RetailerController extends Controller
 			'brand.*' => 'sometimes|exists:brands,id',
 		]);
 	}
-	
-  public function filter($query)
+
+	public function filter($retailer)
 	{
+		$query = $retailer->products();
 		$request = request();
 		$filters = $this->validateFilters();
 		foreach ($filters as $field => $values) {
 			$query->whereIn("{$field}_id", $values);
 		}
 		$products = $query->get();
-		if ($request->input('show_available_only')) {
-			$products = $products->filter(function ($item) {
-				return $item->retails->isNotEmpty();
-			});
-		}
-
-		# admin filters
-		if (($user = auth()->user()) && ($user->isSuperAdmin())) {
-			if ($request->input('show_empty_only')) {
-				$products = $products->filter(function ($item) {
-					return $item->images->isEmpty();
-				});
-			} elseif ($request->input('show_not_empty_only')) {
-				$products = $products->filter(function ($item) {
-					return $item->images->isNotEmpty();
-				});
-			}
-			if ($vendors = $request->input('vendor')) {
-				$products = $products->filter(function ($item) use ($vendors) {
-					return $item->offer->whereIn('vendor_id', $vendors)->isNotEmpty();
-				});
-			}
-		} else {
-			$products = $products->filter(function ($item) {
-				return $item->images->isNotEmpty();
-			});
-		}
-
-		# vendor filters
-		if ($user && ($vendor = $user->vendor) && $request->input('show_my_stock_only')) {
-			$products = $products->filter(function ($item) use ($vendor) {
-				return $item->offer->where('vendor_id', $vendor->id)->isNotEmpty();
-			});
-		}
+		$user = auth()->user();
+		$products->load(['retails' => function ($query) use ($retailer) {
+			$query->where('retailer_id', $retailer->id);
+		}, 'image', 'brand']);
+		$products = $products->filter(function ($item) {
+			return $item->retails->isNotEmpty() && $item->image;
+		});
 		return $products;
 	}
-	
+
   public function sortOptions()
 	{
 		return ['default', 'random','price-high-to-low','price-low-to-high','hottest','best-selling','newest','oldest'];
 	}
-	
+
   public function filterOptions()
 	{
 		return [
@@ -159,7 +104,7 @@ class RetailerController extends Controller
 			"brand" => \App\Brand::all()
 		];
 	}
-	
+
   public function sort($products)
 	{
 		if (request()->input('sort')) {
@@ -183,13 +128,13 @@ class RetailerController extends Controller
 
 			case 'price-high-to-low':
 				$products = $products->sortByDesc(function ($item) {
-					return $item->getMinPrice('retail', 0);
+					return $item->getMinPrice(0);
 				});
 				break;
 
 			case 'price-low-to-high':
 				$products = $products->sortBy(function ($item) {
-					return $item->getMinPrice('retail', INF);
+					return $item->getMinPrice(INF);
 				});
 				break;
 
