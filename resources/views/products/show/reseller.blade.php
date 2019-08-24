dbd3771af758c3fc321e73c14bdab51e4c6300cf
Dope <yjuyjuy@gmail.com>
2019-08-24 18:20:53 -0500

    added text to indicate if no offer is available

--- resources/views/products/show/reseller.blade.php
+++ resources/views/products/show/reseller.blade.php
@@ -1,10 +1,10 @@
-@if($product->offers->isNotEmpty())
 <div class="d-flex flex-column products-show__info--vendor">
-	<div class="d-flex flex-column">
+	@if($product->offers->isNotEmpty())
 		<span class="">{{ __('Order price') }}</span>
-		@foreach($product->getSizePrice('offer') as $size => $data)
-			<span class="">{{$size}} - &yen;{{$data['price']}} - {{$data['vendor']}}</span>
-		@endforeach
-	</div>
+			@foreach($product->getSizePrice('offer') as $size => $data)
+				<div><a href="{{ $data['link'] }}" class="">{{$size}} - &yen;{{$data['price']}} - {{$data['vendor']}}</a></div>
+			@endforeach
+		@else
+			<span class="not-available">{{ __('no offer') }}</span>
+		@endif
 </div>
-@endif
