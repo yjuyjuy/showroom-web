dd3c45a070c1da7db1fbc67bf30678de7d630316
Dope <yjuyjuy@gmail.com>
2019-08-24 18:16:24 -0500

    removed mdc-button--error css class on close dialog button

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -47,7 +47,7 @@
 	      </div>
 
 	      <footer class="mdc-dialog__actions">
-	        <button type="button" class="mdc-button mdc-button--error mdc-dialog__button" data-mdc-dialog-action="close">
+	        <button type="button" class="mdc-button mdc-dialog__button" data-mdc-dialog-action="close">
 	          <span class="mdc-button__label">{{ __('close') }}</span>
 	        </button>
 					<button type="button" class="mdc-button mdc-dialog__button"
