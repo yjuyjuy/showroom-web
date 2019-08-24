84780f5647d2e487f2e44f831745e752e2aa793a
Dope <yjuyjuy@gmail.com>
2019-08-24 17:49:05 -0500

    On users table, added wechat_id column, changed is_reseller column to type column to allow pending, rejected, null, reseller values,

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,11 @@ class CreateUsersTable extends Migration
 			$table->bigIncrements('id');
 			$table->string('username')->unique();
 			$table->string('email')->unique();
+			$table->string('wechat_id')->unique()->nullable();
 			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password');
 			$table->unsignedBigInteger('vendor_id')->nullable();
-			$table->boolean('is_reseller');
+			$table->string('type')->nullable();
 			$table->rememberToken();
 			$table->timestamps();
 
