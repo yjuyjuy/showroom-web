157618324516007fb8d26fb0ffa8ee9d3919061c
Dope <yjuyjuy@gmail.com>
2019-08-24 17:51:06 -0500

    added settings.edit view

---
+++ resources/views/account/settings/edit.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', '账户设置 - '.$user->name)
+
+@section('content')
+<div class="fullscreen-center">
+	<div class="mdc-typography--headline5 text-center mx-3 mt-n5">
+		更多高级选项陆续开放中
+	</div>
+</div>
+@endsection
