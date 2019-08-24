2ff1370282b5b425f1aa5dcb9793706f4ad705bf
Dope <yjuyjuy@gmail.com>
2019-08-24 17:39:20 -0500

    added requests(), agree(), reject() methods for handling user account upgrade requests

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -134,4 +134,24 @@ class AdminController extends Controller
 			$vendorPrice->save();
 		}
 	}
+
+	public function requests()
+	{
+		$users = \App\User::where('type', 'pending')->get();
+		return view('admin.requests', compact('users'));
+	}
+	public function agree(Request $request)
+	{
+		$user = \App\User::find($request->validate(['user_id' => 'exists:users,id'])['user_id']);
+		$user->is_reseller = true;
+		$user->save();
+		return ['success'];
+	}
+	public function reject(Request $request)
+	{
+		$user = \App\User::find($request->validate(['user_id' => 'exists:users,id'])['user_id']);
+		$user->is_rejected = true;
+		$user->save();
+		return ['success'];
+	}
 }
