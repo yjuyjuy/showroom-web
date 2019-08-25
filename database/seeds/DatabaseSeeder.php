28018aeb8c161b737007b4500d28f12d1dd1f86c
Dope <yjuyjuy@gmail.com>
2019-08-24 23:33:55 -0500

    added more testing accounts

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,23 +11,21 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run()
 	{
-		$user = \App\User::where('email','admin@dopebxtch.com')->first();
-		$user->password = Hash::make('iZ92pa9knDN3taT');
-		$user->save();
+		\App\User::where('email', 'yjuyjuy@gmail.com')->update(['password' => Hash::make('Tptz9kKZ48rTefK'),]);
+		\App\User::where('email', 'admin@dopebxtch.com')->update(['password' => Hash::make('iZ92pa9knDN3taT'),]);
+		\App\User::where('email', 'user@dopebxtch.com')->update(['password' => Hash::make('9SckaJsgDHiznAh'),]);
+		\App\User::where('email', 'vendor@dopebxtch.com')->update(['password' => Hash::make('Ku47dCJwrQKP7hv'),]);
+		\App\User::where('email', 'reseller@dopebxtch.com')->update(['password' => Hash::make('5U2U5H8upHzAZrv'),]);
 
-		$admin = \App\User::where('email', 'yjuyjuy@gmail.com')->first();
-		$admin->password = Hash::make('Tptz9kKZ48rTefK');
-		$admin->save();
-		
 		// $admin->is_reseller = true;
 		// $admin->save();
 		// $vendor = \App\Vendor::where('name', 'Dopebxtch')->first();
 		// $vendor->users()->save($admin);
-		// 
+		//
 		// if($farfetch_vendor = \App\Vendor::where('name','Farfetch')->first()) {
 		// 	$farfetch_vendor->delete();
 		// }
-		// 
+		//
 		// foreach(\App\Vendor::all() as $vendor){
 		// 	if(!$vendor->retailer){
 		// 		$retailer = new \App\Retailer(['name' => $vendor->name, 'id' => $vendor->id]);
@@ -36,14 +34,14 @@ class DatabaseSeeder extends Seeder
 		// 		$vendor->save();
 		// 	}
 		// }
-		// 
+		//
 		// if(!\App\Retailer::where('name', 'Farfetch')->first()){
 		// 	$retailer = new \App\Retailer();
 		// 	$retailer->name = "Farfetch";
 		// 	$retailer->homepage = "https://www.farfetch.com/";
 		// 	$retailer->save();
 		// }
-		// 
+		//
 		// $admin->vendors()->sync(\App\Vendor::all()->pluck('id'));
 		// $admin->following()->sync(\App\Retailer::all()->pluck('id'));
 		// $admin->vendor->retailer->partner_vendors()->sync(\App\Vendor::where('id','<>',$admin->vendor->id)->get()->pluck('id'));
