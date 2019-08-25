e33e98761c22baad9f59d401d47208304606c497
Dope <yjuyjuy@gmail.com>
2019-08-25 03:13:25 -0500

    test weixin://

--- app/RetailPrice.php
+++ app/RetailPrice.php
@@ -30,4 +30,8 @@ class RetailPrice extends Model
 	{
 		 return $this->morphMany(Log::class, 'price');
 	}
+	public function getLinkAttribute()
+	{
+		return $this->attributes['link'] ?? 'weixin://';
+	}
 }
