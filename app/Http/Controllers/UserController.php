36ee62e48ec2a39da89e588cc513690e730916ef
Dope <yjuyjuy@gmail.com>
2019-08-25 23:34:42 -0500

    moved UserController::following_vendors() to VendorController::following()

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,29 +26,6 @@ class UserController extends Controller
 		}
 		return redirect(route('account.status'));
 	}
-
-	public function following_vendors(Request $request)
-	{
-		$user = auth()->user();
-		$message = false;
-
-		if ($request->input('wechat_id')) {
-			$wechat_id = $request->validate([
-				'wechat_id' => ['sometimes', 'string', 'max:255'],
-			])['wechat_id'];
-			$vendor = \App\Vendor::where('wechat_id', $wechat_id)->first();
-			if (!$vendor) {
-				$message = '没有找到微信号是"'.$wechat_id.'"的同行';
-			} elseif ($user->following_vendors->contains($vendor)) {
-				$message = '已经关注"'.$vendor->name.'"了';
-			} else {
-				$user->following_vendors()->attach($vendor);
-				$message = '成功添加关注"'.$vendor->name.'"';
-			}
-		}
-		$vendors = $user->fresh()->following_vendors;
-		return view('vendors.following', compact('vendors', 'message'));
-	}
 	public function edit()
 	{
 		$user = auth()->user();
