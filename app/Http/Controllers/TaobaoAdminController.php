5bd237fb29563f6d7a10054df4534e94ab3eaec4
Dope <yjuyjuy@gmail.com>
2019-08-27 18:47:30 -0500

    finished TaobaoAdminController

--- app/Http/Controllers/TaobaoAdminController.php
+++ app/Http/Controllers/TaobaoAdminController.php
@@ -10,12 +10,74 @@ use Illuminate\Http\Request;
 
 class TaobaoAdminController extends Controller
 {
+	public function index()
+	{
+		$shops = \App\TaobaoShop::all();
+		return view('taobao.admin.index', compact('shops'));
+	}
+
+	public function admin(TaobaoShop $shop)
+	{
+		$this->authorize('update', $shop);
+		return view('taobao.admin.admin', compact('shop'));
+	}
+
+	public function links(TaobaoShop $shop = null)
+	{
+		if (!$shop) {
+			$prices = \App\TaobaoPrice::whereNotNull('prices')->whereNull('product_id')->where('ignore', false)->orderBy('taobao_id')->get();
+		} else {
+			$prices = $shop->prices()->whereNotNull('prices')->whereNull('product_id')->where('ignore', false)->orderBy('taobao_id')->get();
+		}
+		return view('taobao.admin.links', compact('prices', 'shop'));
+	}
+
+	public function linked(TaobaoShop $shop)
+	{
+		$prices = $shop->prices()->whereNotNull('prices')->whereNotNull('product_id')->where('ignore', false)->orderBy('updated_at', 'desc')->get();
+		return view('taobao.admin.links', compact('prices', 'shop'));
+	}
+
+	public function ignored(TaobaoShop $shop)
+	{
+		$prices = $shop->prices()->whereNotNull('prices')->where('ignore', true)->get();
+		return view('taobao.admin.links', compact('prices', 'shop'));
+	}
+
+	public function diffs(TaobaoShop $shop)
+	{
+		if ($shop->is_partner) {
+			$diffs = collect();
+			$taobao_prices = $shop->prices()->whereNotNull('product_id')->whereNotNull('prices')->where('ignore', false)->with(['product', 'product.image'])->get();
+			$retail_prices = \App\RetailPrice::where('retailer_id', $shop->retailer_id)->orderBy('updated_at')->with(['product', 'product.image'])->get();
+			foreach ($retail_prices as $retail) {
+				$taobao_price = $taobao_prices->where('product_id', $retail->product_id)->first();
+				if (!$taobao_price) {
+					// 需要上架
+					$diffs->push(['retail' => $retail, 'taobao' => null, 'product' => $retail->product]);
+				} elseif ($retail->prices != $taobao_price->prices) {
+					// 需要修改
+					$diffs->push(['retail' => $retail, 'taobao' => $taobao_price, 'product' => $retail->product]);
+				}
+			}
+			foreach ($taobao_prices as $price) {
+				if ($retail_prices->where('product_id', $price->product_id)->isEmpty()) {
+					// 需要下架
+					$diffs->push(['retail' => null, 'taobao' => $price, 'product' => $price->product]);
+				}
+			}
+			return view('taobao.admin.diffs', compact('shop', 'diffs'));
+		} else {
+			abort(403);
+		}
+	}
+
 	public function link(Request $request)
 	{
 		$data = $request->validate([
-			'product_id' => 'exists:mysql.products,id',
-			'price_id' => 'exists:taobao.prices,id',
-		]);
+				'product_id' => 'exists:mysql.products,id',
+				'price_id' => 'exists:taobao.prices,id',
+			]);
 		$price = TaobaoPrice::find($data['price_id']);
 		$this->authorize('update', $price->shop);
 		$price->product_id = $data['product_id'];
@@ -23,21 +85,21 @@ class TaobaoAdminController extends Controller
 		$price->save();
 		if (!$price->shop->is_partner) {
 			$retail = \App\Retail::firstOrNew([
-					'retailer_id' => $price->shop->retailer_id,
-					'product_id' => $price->product_id,
-				]);
+						'retailer_id' => $price->shop->retailer_id,
+						'product_id' => $price->product_id,
+					]);
 			$retail->prices = $price->prices;
 			$retail->save();
 		}
 		return ['success' => true,];
 	}
-		
+
 	public function unlink(Request $request)
 	{
 		$data = $request->validate([
-			'product_id' => 'exists:mysql.products,id',
-			'price_id' => 'exists:taobao.prices,id',
-		]);
+				'product_id' => 'exists:mysql.products,id',
+				'price_id' => 'exists:taobao.prices,id',
+			]);
 		$price = TaobaoPrice::find($data['price_id']);
 		$this->authorize('update', $price->shop);
 		if ($price->product_id == $data['product_id']) {
@@ -58,86 +120,12 @@ class TaobaoAdminController extends Controller
 	public function ignore(Request $request)
 	{
 		$data = $request->validate([
-			'price_id' => 'exists:taobao.prices,id',
-		]);
+				'price_id' => 'exists:taobao.prices,id',
+			]);
 		$price = TaobaoPrice::find($data['price_id']);
 		$this->authorize('update', $price->shop);
 		$price->ignore = true;
 		$price->save();
 		return ['success' => true,];
-	}
-	
-	public function reset(Request $request)
-	{
-		$data = $request->validate([
-			'price_id' => 'exists:taobao.prices,id',
-		]);
-		$price = TaobaoPrice::find($data['price_id']);
-		$this->authorize('update', $price->shop);
-		$price->ignore = false;
-		$price->save();
-		return ['success' => true,];
-	}
-	
-	public function shops()
-	{
-		$shops = \App\TaobaoShop::all();
-		return view('taobao.shops', compact('shops'));
-	}
-	
-	public function admin(TaobaoShop $shop)
-	{
-		$this->authorize('update', $shop);
-		return view('taobao.admin', compact('shop'));
-	}
-
-	public function links(TaobaoShop $shop = null)
-	{
-		if (!$shop) {
-			$prices = \App\TaobaoPrice::whereNotNull('prices')->whereNull('product_id')->where('ignore', false)->get();
-		} else {
-			$prices = $shop->prices()->whereNotNull('prices')->whereNull('product_id')->where('ignore', false)->get();
-		}
-		return view('taobao.links', compact('prices'));
-	}
-
-	public function linked(TaobaoShop $shop)
-	{
-		$prices = $shop->prices()->whereNotNull('prices')->whereNotNull('product_id')->where('ignore', false)->get();
-		return view('taobao.links', compact('prices'));
-	}
-	
-	public function ignored(TaobaoShop $shop)
-	{
-		$prices = $shop->prices()->whereNotNull('prices')->where('ignore', true)->get();
-		return view('taobao.links', compact('prices'));
-	}
-
-	public function diffs(TaobaoShop $shop)
-	{
-		if ($shop->is_partner) {
-			$diffs = [];
-			$taobao_prices = $shop->prices()->whereNotNull('product_id')->whereNotNull('prices')->where('ignore', false)->get();
-			$retail_prices = \App\RetailPrice::where('retailer_id', $shop->retailer_id)->get();
-			foreach ($taobao_prices->load('product', 'product.image') as $price) {
-				$retail = $retail_prices->where('product_id', $price->product_id)->first();
-				if (!$retail) {
-					// 需要下架
-					$diffs[] = ['retail' => null, 'taobao' => $price, 'product' => $price->product];
-				} elseif ($retail->prices != $price->prices) {
-					// 需要修改
-					$diffs[] = ['retail' => $retail, 'taobao' => $price, 'product' => $price->product];
-				}
-			}
-			foreach ($retail_prices->load('product') as $retail) {
-				if (!$taobao_prices->where('product_id', $retail->product_id)->first()) {
-					// 需要上架
-					$diffs[] = ['retail' => $retail, 'taobao' => null, 'product' => $retail->product];
-				}
-			}
-			return view('taobao.diffs', compact('shop', 'diffs'));
-		} else {
-			abort(403);
-		}
 	}
 }
