4dce263a76f2d1693e0083a53bb5078af9f7bcc8
Dope <yjuyjuy@gmail.com>
2019-08-27 21:14:56 -0500

    deleted observers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,10 +25,6 @@ class AppServiceProvider extends ServiceProvider
 	{
 		\App\User::observe(\App\Observers\UserObserver::class);
 		\App\Product::observe(\App\Observers\ProductLogger::class);
-		// \App\OfferPrice::observe(\App\Observers\OfferPriceObserver::class);
-		// \App\OfferPrice::observe(\App\Observers\OfferPriceLogger::class);
-		// \App\RetailPrice::observe(\App\Observers\RetailPriceObserver::class);
-		// \App\RetailPrice::observe(\App\Observers\RetailPriceLogger::class);
 		\App\VendorPrice::observe(\App\Observers\PriceObserver::class);
 	}
 }
