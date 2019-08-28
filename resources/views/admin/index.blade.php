25de66f9099b61ccf7bdd7e0a728e56403f607dc
Dope <yjuyjuy@gmail.com>
2019-08-27 20:58:50 -0500

    added fullscreen-center class on container div

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', '手动执行程序 - admin')
 
 @section('content')
-<div class="d-flex justify-content-center w-100">
+<div class="fullscreen-center">
 	<div class="d-flex flex-column text-center">
 		@foreach($functions as $name => $description)
 		<div class="my-4 mdc-typography--headline5"><a href="{{ route('admin.call', ['function' => $name,]) }}">{{ $description }}</a></div>
