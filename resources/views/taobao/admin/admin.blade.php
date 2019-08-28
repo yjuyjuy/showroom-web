6fe4aad372320da35de5c14ba191c13790830963
Dope <yjuyjuy@gmail.com>
2019-08-27 21:01:22 -0500

    only shows diffs route to partner taobao admin

--- resources/views/taobao/admin/admin.blade.php
+++ resources/views/taobao/admin/admin.blade.php
@@ -6,7 +6,7 @@
 	<div class="d-flex flex-column mt-n4">
 		<div class="text-center my-4"><a href="{{ route('taobao.products.index', ['shop' => $shop,]) }}" class="mdc-typography--headline5">店铺首页</a></div>
 		<div class="text-center my-4"><a href="{{ route('taobao.admin.links', ['shop' => $shop,]) }}" class="mdc-typography--headline5">关联商品</a></div>
-		<div class="text-center my-4"><a href="{{ route('taobao.admin.diffs', ['shop' => $shop,]) }}" class="mdc-typography--headline5">检测价格</a></div>
+		@if($shop->is_partner)<div class="text-center my-4"><a href="{{ route('taobao.admin.diffs', ['shop' => $shop,]) }}" class="mdc-typography--headline5">检测价格</a></div>@endif
 	</div>
 </div>
 @endsection
