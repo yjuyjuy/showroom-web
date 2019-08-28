fcd5e2161d77dbbba82ec90d22daff82b48d761d
Dope <yjuyjuy@gmail.com>
2019-08-27 22:16:31 -0500

    fixed missing route parameters in layouts.pages view

--- resources/views/layouts/pages.blade.php
+++ resources/views/layouts/pages.blade.php
@@ -1,11 +1,11 @@
 <div class="d-flex w-100 justify-content-center">
 		@if($page > 1)
-		<a href="{{ route(Route::currentRouteName(), array_merge(request()->query(), ['page' => $page - 1,])) }}" class="mdc-button">
+		<a href="{{ route(Route::currentRouteName(), array_merge(request()->query(), ['vendor' => $vendor??null, 'retailer' => $retailer??null, 'page' => $page - 1,])) }}" class="mdc-button">
 			<span class="mdc-button__label">上一页</span>
 		</a>
 		@endif
 		@if($page < $total_pages)
-		<a href="{{ route(Route::currentRouteName(), array_merge(request()->query(), ['page' => $page + 1,])) }}" class="mdc-button">
+		<a href="{{ route(Route::currentRouteName(), array_merge(request()->query(), ['vendor' => $vendor??null, 'retailer' => $retailer??null, 'page' => $page + 1,])) }}" class="mdc-button">
 			<span class="mdc-button__label">下一页</span>
 		</a>
 		@endif
