7adee0268a870a12c1c732f915be55886e5f851a
Dope <yjuyjuy@gmail.com>
2019-08-28 21:59:09 -0500

    use a 0 opacity fixed position input box to workaround iOS copy to clipboard requirement

--- resources/views/products/show/reseller.blade.php
+++ resources/views/products/show/reseller.blade.php
@@ -3,13 +3,16 @@
 	@if($product->offers->isNotEmpty())
 		<div class="">
 			<span class="">{{ __('Order price') }}</span>
-			<button type="button" class="mdc-button" onclick='copy_to_clipboard(@json($product->offers_to_string))'>
-				<span class="mdc-button__label">复制尺码价格</span>
-			</button>
 		</div>
 		@foreach($product->getSizePrice('offer') as $size => $data)
 		<span>{{$size}} - &yen;{{$data['price']}} - {{$data['vendor']}}</span>
 		@endforeach
+		<div class="">
+			<input type="text" value="{{ $product->offers_to_string }}" style="opacity:0;position:fixed;">
+			<button type="button" class="mdc-button" onclick='var input = this.parentElement.firstChild;input.setSelectionRange(0,input.value.length);document.execCommand("cut");input.blur();'>
+				<span class="mdc-button__label">复制尺码价格</span>
+			</button>
+		</div>
 	@else
 	<span>{{ __('no offer') }}</span>
 	@endif
