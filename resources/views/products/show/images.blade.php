cba880db328cf8d04c492605e76c44d6ee031943
Dope <yjuyjuy@gmail.com>
2019-08-28 22:00:54 -0500

    edit text under image slider

--- resources/views/products/show/images.blade.php
+++ resources/views/products/show/images.blade.php
@@ -1,10 +1,9 @@
 
 <images-slider :images='@json($product->images->pluck('url')->toArray())'></images-slider>
 <div class="w-100 text-center">
+	<span>提示: 长按小图保存图片</span>
 	@can('update',\App\Image::class)
 		<a href="{{route('images.edit',['product'=>$product])}}"
 			 onclick="event.preventDefault(); window.location.replace(this.href);">管理图片</a>
-	@else
-		<span>(长按小图保存图片)</span>
 	@endcan
 </div>
