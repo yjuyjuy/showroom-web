a54fb18ae648629ae5ec779f7bdc33290ca0952d
Dope <yjuyjuy@gmail.com>
2019-08-28 22:07:42 -0500

    working in desktop chrome

--- resources/views/products/show/reseller.blade.php
+++ resources/views/products/show/reseller.blade.php
@@ -7,9 +7,9 @@
 		@foreach($product->getSizePrice('offer') as $size => $data)
 		<span>{{$size}} - &yen;{{$data['price']}} - {{$data['vendor']}}</span>
 		@endforeach
-		<div class="">
+		<div class="mt-2">
 			<input type="text" value="{{ $product->offers_to_string }}" style="opacity:0;position:fixed;">
-			<button type="button" class="mdc-button" onclick='var input = this.parentElement.firstChild;input.setSelectionRange(0,input.value.length);document.execCommand("cut");input.blur();'>
+			<button type="button" class="mdc-button" onclick="var input = this.parentElement.firstChild;input.setSelectionRange(0,input.value.length);input.select();document.execCommand('copy');input.blur();">
 				<span class="mdc-button__label">复制尺码价格</span>
 			</button>
 		</div>
