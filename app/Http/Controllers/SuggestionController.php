f28a43f799a1de350ca6b3ab79afe87bc03a77a3
Dope <yjuyjuy@gmail.com>
2019-09-03 17:50:25 -0500

    added SuggestionController

--- app/Http/Controllers/SuggestionController.php
+++ app/Http/Controllers/SuggestionController.php
@@ -7,17 +7,24 @@ use App\Suggestion;
 
 class SuggestionController extends Controller
 {
+  public function index()
+  {
+    $suggestions = \App\Suggestion::whereNull('status')->get();
+    return view('admin.suggestions', compact('suggestions'));
+  }
+  
   public function create()
   {
   	return view('suggestion.create');
   }
-	public function store(Request $request)
+	
+  public function store(Request $request)
   {
 		$data = $request->validate([
 			'category' => 'sometimes|string|in:'.implode(',',$this->categories()),
 			'content' => 'required|string|max:255',
 		]);
-		Suggestion::create($data);
+		\App\Suggestion::create($data);
   	return view('suggestion.stored');
   }
 	public function categories()
