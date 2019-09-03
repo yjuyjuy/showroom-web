754eaabfb2921aa6a7f6f6f778e02b8d932c8471
Dope <yjuyjuy@gmail.com>
2019-09-03 18:08:16 -0500

    added use App\Model

--- app/Http/Controllers/FollowRetailerController.php
+++ app/Http/Controllers/FollowRetailerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Retailer;
 use Illuminate\Http\Request;
 
 class FollowRetailerController extends Controller
--- app/Http/Controllers/FollowVendorController.php
+++ app/Http/Controllers/FollowVendorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Vendor;
 use Illuminate\Http\Request;
 
 class FollowVendorController extends Controller
