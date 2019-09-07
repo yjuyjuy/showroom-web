94f4cb78aa43a52143b65c5c7aa55e32463eb982
Dope <yjuyjuy@gmail.com>
2019-09-07 01:06:20 -0500

    delete text

--- resources/views/products/show/images.blade.php
+++ resources/views/products/show/images.blade.php
@@ -1,7 +1,6 @@
 
 <carousel :images='@json($product->images->pluck('url')->toArray())'></carousel>
 <div class="w-100 text-center">
-	<span>提示: 长按小图保存图片</span>
 	@can('update',\App\Image::class)
 		<a href="{{route('images.edit',['product'=>$product])}}">管理图片</a>
 	@endcan
