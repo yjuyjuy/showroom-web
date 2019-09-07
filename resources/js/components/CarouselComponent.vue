9dca4e72187bac86d32814138ed615214df06a79
Dope <yjuyjuy@gmail.com>
2019-09-06 21:29:53 -0500

    added CarouselComponent

---
+++ resources/js/components/CarouselComponent.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="carousel">
+	
+	<div class="carousel__content">		
+		<a v-for="(image,index) in images" class="carousel-item" v-bind:class="{ active: index == active }" download :href="image" @click="next_item()">
+			<img :src="image" class="d-block w-100">
+		</a>
+	</div>
+	
+	<div class="carousel__controls">
+		<button type="button" class="mdc-icon-button material-icons carousel__control-prev" @click="prev_item()">navigate_before</button>
+		<button type="button" class="mdc-icon-button material-icons carousel__control-next" @click="next_item()">navigate_next</button>
+	</div>
+	
+</div>
+</template>
+
+<script>
+export default {
+	props: {
+		images: Array,
+	},
+	data: function() {
+		return {
+			active: 0,
+		};
+	},
+	beforeMount: function() {
+		
+	},
+	mounted: function() {},
+	watch: {},
+	methods: {
+		prev_item: function() {
+			this.active -= 1;
+			if (this.active < 0) {
+				this.active = this.images.length - 1;
+			}
+		},
+		next_item: function() {
+			this.active += 1;
+			if (this.active >= this.images.length) {
+				this.active = 0;
+			}
+		},
+	}
+}
+</script>
