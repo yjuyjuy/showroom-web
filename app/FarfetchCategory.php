671a790acb5d4fe246c6c2c264943d53005d0429
Surface Laptop 3 <yjuyjuy@gmail.com>
2019-10-02 10:00:15 +0800

    use url_token as route key, prefix description with gender name

--- app/FarfetchCategory.php
+++ app/FarfetchCategory.php
@@ -18,9 +18,28 @@ class FarfetchCategory extends Model
 	 * @var string
 	 */
 	protected $table = 'categories';
-
+	/**
+	 * Get the route key for the model.
+	 *
+	 * @return string
+	 */
+	public function getRouteKeyName()
+	{
+		return 'url_token';
+	}
 	public function products()
 	{
-		return $this->belongsToMany(FarfetchProduct::class,'category_product','category_id','product_id');
+		return $this->hasMany(FarfetchProduct::class, 'category_id');
+	}
+	public function getDescriptionAttribute()
+	{
+		$description = $this->attributes['description'];
+		if (strpos($this->url_token, '-1')) {
+			return '女装-'.$description;
+		} elseif (strpos($this->url_token, '-2')) {
+			return '男装-'.$description;
+		} else {
+			return $description;
+		}
 	}
 }
