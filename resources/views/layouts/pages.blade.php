cf8c95a0367c5d9bef8dfd439e83e644a7f27f6c
Surface Laptop 3 <yjuyjuy@gmail.com>
2019-10-02 10:11:44 +0800

    added $designer, $category to $route_params array

--- resources/views/layouts/pages.blade.php
+++ resources/views/layouts/pages.blade.php
@@ -1,11 +1,17 @@
 <div class="d-flex w-100 justify-content-center">
+	<?php $route_params = [
+			'vendor' => $vendor ?? null,
+			'retailer' => $retailer ?? null,
+			'designer' => $designer ?? null,
+			'category' => $category ?? null,
+		]; ?>
 		@if($page > 1)
-		<a href="{{ route(Route::currentRouteName(), array_merge(request()->query(), ['vendor' => $vendor??null, 'retailer' => $retailer??null, 'page' => $page - 1,])) }}" class="mdc-button">
+		<a href="{{ route(Route::currentRouteName(), array_merge(request()->query(), $route_params, ['page' => $page - 1,])) }}" class="mdc-button">
 			<span class="mdc-button__label">上一页</span>
 		</a>
 		@endif
 		@if($page < $total_pages)
-		<a href="{{ route(Route::currentRouteName(), array_merge(request()->query(), ['vendor' => $vendor??null, 'retailer' => $retailer??null, 'page' => $page + 1,])) }}" class="mdc-button">
+		<a href="{{ route(Route::currentRouteName(), array_merge(request()->query(), $route_params, ['page' => $page + 1,])) }}" class="mdc-button">
 			<span class="mdc-button__label">下一页</span>
 		</a>
 		@endif
