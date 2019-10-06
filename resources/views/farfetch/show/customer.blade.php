74d0dd43ba4d90b08c7f1163a2729f879d44654c
Surface Laptop 3 <yjuyjuy@gmail.com>
2019-10-06 17:29:03 +0800

    add export buttons in view templates

--- resources/views/farfetch/show/customer.blade.php
+++ resources/views/farfetch/show/customer.blade.php
@@ -11,4 +11,13 @@
 	<a href="{{ $product->url }}" class="ml-2 mdc-button mdc-button--unelevated" target="_blank">
 		<span class="mdc-button__label">{{ __('Link to page') }}</span>
 	</a>
+	@if(auth()->user()->isSuperAdmin())
+		<a href="{{ route('farfetch.export', ['farfetch_product' => $product,]) }}" class="ml-2 mdc-button mdc-button--unelevated">
+			@if(\App\Product::where('designer_style_id', $product->designer_style_id)->where('brand_id', $product->mapped_brand_id)->count() <= 0)
+			<span class="mdc-button__label">上架</span>
+			@else
+			<span class="mdc-button__label">导入</span>
+			@endif
+		</a>
+	@endif
 </div>
