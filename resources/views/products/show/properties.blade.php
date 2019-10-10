9f667168e4d17f8f14e32f5f342e4b9af4469987
Surface Laptop 3 <yjuyjuy@gmail.com>
2019-10-10 12:00:22 +0800

    use ?? to avoid error accessing null properties

--- resources/views/products/show/properties.blade.php
+++ resources/views/products/show/properties.blade.php
@@ -2,7 +2,9 @@
 
 	<div class="mdc-typography--headline6">
 		<a id="product-brand" href="{{ route('products.index',['brand' => [$product->brand_id,]]) }}">
-			{{ __($product->brand->full_name) }}</a>
+			{{ __($product->brand->full_name ?? '') }}</a>
+		<a id="product-brand" href="{{ route('products.index',['category' => [$product->category_id,]]) }}">
+			{{ __($product->category->name ?? '') }}</a>
 	</div>
 	<div class="mdc-typography--headline6">
 		<a class="product-name" href="{{ route('products.show',['product' => $product]) }}">
@@ -12,8 +14,11 @@
 
 	@can('update',$product)
 	<div class="mt-2">
-		<div style="text-transform:capitalize;">{{ $product->name }} {{ __($product->color->name) }}</div>
-		@if($product->designer_style_id)<div class="">{{ __('designer_style_id') }}: {{$product->designer_style_id}}</div>@endif
+		<div style="text-transform:capitalize;">{{ $product->name }} {{ __($product->color->name ?? '') }}</div>
+		<div class="">
+			@if($product->season_id)<span class="">{{$product->season->name}} {{ __('season') }}</span>@endif
+			@if($product->designer_style_id)<span class="">{{ __('designer_style_id') }}: {{$product->designer_style_id}}</span>@endif
+		</div>
 		<div class="">
 			<span class="">ID: {{ $product->id }}</span>
 			<a href="{{ route('products.edit',['product' => $product ]) }}">{{ __('edit') }}</a>
