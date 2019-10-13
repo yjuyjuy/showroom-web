e67709d46e95ae1d4e5674fcf75435887add7886
Surface Laptop 3 <yjuyjuy@gmail.com>
2019-10-13 14:43:12 +0800

    fix clickable area in select vendor menu

--- resources/views/products/show/admin.blade.php
+++ resources/views/products/show/admin.blade.php
@@ -33,9 +33,7 @@
 			<div class="mdc-menu mdc-menu-surface mdc-menu--with-button">
 			  <ul class="mdc-list" role="menu" aria-hidden="true" aria-orientation="vertical" tabindex="-1">
 			    @foreach(\App\Vendor::whereNotIn('id',$product->prices->pluck('vendor_id')->toArray())->get() as $vendor)
-					<li class="mdc-list-item" role="menuitem">
-			      <a href="{{route('prices.create',['product' => $product, 'vendor' => $vendor])}}" class="mdc-list-item__text w-100 text-left">{{$vendor->name}}</a>
-			    </li>
+			      <a href="{{route('prices.create',['product' => $product, 'vendor' => $vendor])}}" class="mdc-list-item text-left" role="menuitem">{{$vendor->name}}</a>
 					@endforeach
 			  </ul>
 			</div>
