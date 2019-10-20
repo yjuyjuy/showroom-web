ed97b21bd2e164ecae2766dc20fc43ad4ae9e528
Surface Laptop 3 <yjuyjuy@gmail.com>
2019-10-20 18:00:13 -0500

    clean up sync_taobao()

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -112,15 +112,7 @@ class AdminController extends Controller
 			\App\RetailPrice::where('retailer_id', $shop->retailer_id)->delete();
 			foreach ($shop->prices()->whereNotNull('product_id')->whereNotNull('prices')->get() as $price) {
 				$retail = \App\RetailPrice::firstOrNew(['product_id' => $price->product_id, 'retailer_id' => $shop->retailer_id]);
-				$prices = $price->prices;
-				if ($retail->prices) {
-					foreach ($retail->prices as $size => $value) {
-						if (!array_key_exists($size, $prices) || $value < $prices[$size]) {
-							$prices[$size] = $value;
-						}
-					}
-				}
-				$retail->prices = $prices;
+				$retail->merge($price->prices);
 				$retail->link = $price->url;
 				$retail->save();
 			}
