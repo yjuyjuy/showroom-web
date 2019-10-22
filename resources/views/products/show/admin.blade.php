7c257b5e8f320e9ee53ab30a64bc2004f19d2ef4
Surface Laptop 3 <yjuyjuy@gmail.com>
2019-10-21 22:02:18 -0700

    move farfetch, end links to reseller.blade.php

--- resources/views/products/show/admin.blade.php
+++ resources/views/products/show/admin.blade.php
@@ -23,28 +23,6 @@
 		</div>
 	</div>
 	@endforeach
-	<?php $farfetch_products = \App\FarfetchProduct::where('product_id', $product->id)->get(); ?>
-	<?php $end_products = \App\EndProduct::where('product_id', $product->id)->get(); ?>
-	@if($farfetch_products->isNotEmpty() || $end_products->isNotEmpty())
-	<div class="d-flex justify-content-end">
-		<div class="mdc-menu-surface--anchor">
-			<button type="button" class="mdc-button open-menu-button">
-				<span class="mdc-button__label">{{ __('link') }}</span>
-				<i class="material-icons mdc-button__icon" aria-hidden="true">arrow_drop_down</i>
-			</button>
-			<div class="mdc-menu mdc-menu-surface mdc-menu--with-button">
-				<ul class="mdc-list" role="menu" aria-hidden="true" aria-orientation="vertical" tabindex="-1">
-					@foreach($farfetch_products as $index => $farfetch_product)
-					<a href="{{route('farfetch.show',['product' => $farfetch_product,])}}" class="mdc-list-item mdc-list-item__text text-left" role="menuitem">Farfetch链接{{$index + 1}}</a>
-					@endforeach
-					@foreach($end_products as $index => $end_product)
-						<a href="{{route('end.show',['product' => $end_product,])}}" class="mdc-list-item mdc-list-item__text text-left" role="menuitem">End链接{{$index + 1}}</a>
-					@endforeach
-				</ul>
-			</div>
-		</div>
-	</div>
-	@endif
 	<div class="d-flex justify-content-end">
 		<div class="mdc-menu-surface--anchor">
 			<button type="button" class="mdc-button open-menu-button">
--- resources/views/products/show/reseller.blade.php
+++ resources/views/products/show/reseller.blade.php
@@ -1,4 +1,3 @@
-@if($user->is_reseller)
 <div class="d-flex flex-column products-show__info--vendor">
 	@if($product->offers->isNotEmpty())
 		<div class="">
@@ -16,5 +15,26 @@
 	@else
 	<span>{{ __('no offer') }}</span>
 	@endif
+</div>
+<?php $farfetch_products = \App\FarfetchProduct::where('product_id', $product->id)->get(); ?>
+<?php $end_products = \App\EndProduct::where('product_id', $product->id)->get(); ?>
+@if($farfetch_products->isNotEmpty() || $end_products->isNotEmpty())
+<div class="d-flex justify-content-end">
+	<div class="mdc-menu-surface--anchor">
+		<button type="button" class="mdc-button open-menu-button">
+			<span class="mdc-button__label">{{ __('link') }}</span>
+			<i class="material-icons mdc-button__icon" aria-hidden="true">arrow_drop_down</i>
+		</button>
+		<div class="mdc-menu mdc-menu-surface mdc-menu--with-button">
+			<ul class="mdc-list" role="menu" aria-hidden="true" aria-orientation="vertical" tabindex="-1">
+				@foreach($farfetch_products as $index => $farfetch_product)
+				<a href="{{route('farfetch.show',['product' => $farfetch_product,])}}" class="mdc-list-item mdc-list-item__text text-left" role="menuitem">Farfetch链接{{$index + 1}}</a>
+				@endforeach
+				@foreach($end_products as $index => $end_product)
+					<a href="{{route('end.show',['product' => $end_product,])}}" class="mdc-list-item mdc-list-item__text text-left" role="menuitem">End链接{{$index + 1}}</a>
+				@endforeach
+			</ul>
+		</div>
+	</div>
 </div>
 @endif
