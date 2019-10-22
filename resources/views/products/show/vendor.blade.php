bdeffcd6dedc8d326dc0caf94049c5a0602f4119
Surface Laptop 3 <yjuyjuy@gmail.com>
2019-10-21 22:04:56 -0700

    clean up code

--- resources/views/products/show/vendor.blade.php
+++ resources/views/products/show/vendor.blade.php
@@ -1,5 +1,5 @@
 <div class="d-flex flex-column products-show__info--vendor">
-	@if($product->prices->firstWhere('vendor_id',$vendor->id))
+	@if($price = $product->prices->firstWhere('vendor_id',$vendor->id))
 	<div class="price-grid">
 		<div class="price-grid__header">
 			<span class="price-grid__title">{{ __('My offer') }}</span>
@@ -10,22 +10,20 @@
 			<span class="price-grid__col">{{ __('offer') }}</span>
 			<span class="price-grid__col">{{ __('retail') }}</span>
 		</div>
-		@foreach($product->prices->where('vendor_id',$vendor->id) as $price)
-			@foreach($price->data as $row)
-				<div class="price-grid__row">
-					<span class="price-grid__col">{{ $row['size'] }}</span>
-					<span class="price-grid__col">&yen;{{$row['cost']}}</span>
-					<span class="price-grid__col">&yen;{{$row['offer']}}</span>
-					<span class="price-grid__col">&yen;{{$row['retail']}}</span>
-				</div>
-			@endforeach
-			<div class="price-grid__footer text-right">
-				<a href="{{route('prices.edit',['price'=>$price])}}" class="mdc-button">{{ __('edit') }}</a>
-				<button type="button" class="mdc-button mdc-button--error" onclick="delete_price({{$price->id}})">
-					<span class="mdc-button__label">{{ __('delete') }}</span>
-				</button>
+		@foreach($price->data as $row)
+			<div class="price-grid__row">
+				<span class="price-grid__col">{{ $row['size'] }}</span>
+				<span class="price-grid__col">&yen;{{$row['cost']}}</span>
+				<span class="price-grid__col">&yen;{{$row['offer']}}</span>
+				<span class="price-grid__col">&yen;{{$row['retail']}}</span>
 			</div>
 		@endforeach
+		<div class="price-grid__footer text-right">
+			<a href="{{route('prices.edit',['price'=>$price])}}" class="mdc-button">{{ __('edit') }}</a>
+			<button type="button" class="mdc-button mdc-button--error" onclick="delete_price({{$price->id}})">
+				<span class="mdc-button__label">{{ __('delete') }}</span>
+			</button>
+		</div>
 	</div>
 	@else
 	<div class="d-flex justify-content-end">
