6b8aab9ef70c6c979569f249c8a81ec603961d12
Chris <yjuyjuy@gmail.com>
2019-11-14 11:32:05 +0800

    fix bug

--- app/Http/Controllers/LouisVuittonController.php
+++ app/Http/Controllers/LouisVuittonController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Product;
 use App\LouisVuittonProduct;
 use App\LouisVuittonImage;
+use App\LouisVuittonCategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Validation\Rule;
