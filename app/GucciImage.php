764e1c1093b67ffb5e1fd41dc6b1d4d448e08671
Chris <yjuyjuy@gmail.com>
2019-11-17 14:14:34 +0800

    fix bug

--- app/GucciImage.php
+++ app/GucciImage.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class GucciImage extends Model
 {
-    //
+	/**
+	 * The connection name for the model.
+	 *
+	 * @var string
+	 */
+	protected $connection = 'gucci';
+	/**
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'images';
+
+	public function product()
+	{
+		return $this->belongsTo(GucciProduct::class, 'product_id');
+	}
+
+	public function getUrlAttribute()
+	{
+		if ($this->path) {
+			return secure_asset('storage/'.$this->path);
+		} else {
+			return '';
+		}
+	}
 }
