cacfd383e9d262cd778a3c5bac4ad8d42958e26d
Chris <yjuyjuy@gmail.com>
2019-11-19 21:12:31 +0800

    move product->links to products.show.customer view

--- resources/views/products/show/reseller.blade.php
+++ resources/views/products/show/reseller.blade.php
@@ -16,20 +16,3 @@
 	<span>{{ __('no offer') }}</span>
 	@endif
 </div>
-@if(!empty($product->links))
-<div class="d-flex justify-content-end">
-	<div class="mdc-menu-surface--anchor">
-		<button type="button" class="mdc-button open-menu-button">
-			<span class="mdc-button__label">{{ __('link') }}</span>
-			<i class="material-icons mdc-button__icon" aria-hidden="true">arrow_drop_down</i>
-		</button>
-		<div class="mdc-menu mdc-menu-surface mdc-menu--with-button">
-			<ul class="mdc-list" role="menu" aria-hidden="true" aria-orientation="vertical" tabindex="-1">
-				@foreach($product->links as $description => $url)
-					<a href="{{ $url }}" class="mdc-list-item mdc-list-item__text text-left" role="menuitem">{{ $description }}</a>
-				@endforeach
-			</ul>
-		</div>
-	</div>
-</div>
-@endif
