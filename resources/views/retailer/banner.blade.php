faba5a7f6a8ccac30572bf12f615090c9b084c9c
Chris <yjuyjuy@gmail.com>
2019-11-20 12:29:51 +0800

    fix bug

--- resources/views/retailer/banner.blade.php
+++ resources/views/retailer/banner.blade.php
@@ -1,6 +1,6 @@
 <div class="m-3 d-flex align-items-end">
 	<span class="mdc-typography--headline3">{{ $retailer->name }}</span>
-	@if(auth()->user()->following_retailers->contains($retailer))
+	@if(auth()->user() && auth()->user()->following_retailers->contains($retailer))
 	<button type="button" class="ml-4 mdc-button mdc-button--outlined" onclick="follow('retailer','{{ $retailer->name }}', false)">
 		<span class="mdc-button__label">{{ __('following') }}</span>
 	</button>
