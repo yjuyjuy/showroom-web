064dd30a9cc092beae8f06deec1acc413cea0092
Chris <yjuyjuy@gmail.com>
2019-11-24 22:27:34 +0800

    redirect user to following.retailers after register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
 	 *
 	 * @var string
 	 */
-	protected $redirectTo = '/';
+	protected $redirectTo = '/following/retailers';
 
 	/**
 	 * Create a new controller instance.
