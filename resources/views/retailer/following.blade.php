d13eef5ad134e0b269c5e5404ee19c63c870a043
Chris <yjuyjuy@gmail.com>
2019-11-25 11:33:51 +0800

    fix styling

--- resources/views/retailer/following.blade.php
+++ resources/views/retailer/following.blade.php
@@ -5,10 +5,10 @@
 @section('content')
 <div class="d-flex flex-column">
 
-	<form class="my-5 d-flex justify-content-center align-items-center">
+	<form class="my-3 d-flex justify-content-center align-items-center">
 		<div class="mdc-text-field mdc-text-field--outlined mdc-text-field--no-label">
 			<input type="text" name="search" class="mdc-text-field__input" aria-label="Label" autofocus
-			placeholder="{{ __('Search retailer') }}" autocomplete="off">
+			placeholder="搜索卖家名字/微信号" autocomplete="off">
 			<div class="mdc-notched-outline">
 				<div class="mdc-notched-outline__leading"></div>
 				<div class="mdc-notched-outline__trailing"></div>
@@ -19,12 +19,12 @@
 		</button>
 	</form>
 
-	<div class="my-5 text-center">
-		<span class="mdc-typography--headline6">搜索认识的卖家的名字或微信号关注卖家，成功关注后卖家报价才会在首页显示，默认只显示各品牌官网报价，取消关注后将不会显示</span>
+	<div class="m-3 text-center">
+		<span class="mdc-typography--headline6">注意: 成功关注后卖家报价才会在首页显示，默认只显示各品牌官网报价，取消关注后将不会在其它页面显示卖家价格</span>
 	</div>
 
 	@if($not_found)
-		<div class="my-5 text-center">{{ __('retailer not found') }}"{{ old('search') }}"</div>
+		<div class="my-3 text-center">{{ __('retailer not found') }}"{{ old('search') }}"</div>
 	@endif
 
 	<div class="my-3 d-flex justify-content-center flex-wrap">
