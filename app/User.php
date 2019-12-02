9235d4cfa5091139e71be8aac9748f9bdecf72cf
Chris <yjuyjuy@gmail.com>
2019-12-02 12:27:06 +0800

    add a new job for sending verification email to new users

--- app/User.php
+++ app/User.php
@@ -40,6 +40,7 @@ class User extends Authenticatable implements MustVerifyEmail
 	 * @var array
 	 */
 	protected $attributes = [];
+
 	public function vendor()
 	{
 		return $this->belongsTo(Vendor::class);
@@ -84,5 +85,14 @@ class User extends Authenticatable implements MustVerifyEmail
 	public function isSuperAdmin()
 	{
 		return in_array($this->id, [1111111111, 4021500970, 8888888888]);
+	}
+	/**
+	 * Send the email verification notification.
+	 *
+	 * @return void
+	 */
+	public function sendEmailVerificationNotification()
+	{
+		\App\Jobs\VerifyEmail::dispatch($this);
 	}
 }
