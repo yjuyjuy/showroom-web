78d75bbfba2a8101827a71e9b54cf51de0424d85
Chris <yjuyjuy@gmail.com>
2019-12-06 17:56:37 +0800

    added user hints

--- resources/views/products/show/images.blade.php
+++ resources/views/products/show/images.blade.php
@@ -1,15 +1,25 @@
 
 <carousel :images='@json($product->images->pluck('url')->toArray())'></carousel>
-<div class="w-100 text-center">
+<div class="d-flex justify-content-between">
+	<div class="">
+		<span>左翻街拍图</span>
+	</div>
 	@can('update',\App\Image::class)
+	<div class="">
 		<a href="{{route('images.edit',['product'=>$product])}}">管理图片</a>
+	</div>
 	@endcan
 	@can('create', \App\Image::class)
+	<div class="">
 		<a href="#" onclick="event.preventDefault(); document.getElementById('images-input').click();">上传图片</a>
 		<form id="upload-image-form" class="" action="{{ route('images.store') }}" method="post" style="display:none;">
 			@csrf
 			<input type="text" name="product_id" value="{{ $product->id }}">
 			<input id="images-input" type="file" name="images" form="upload-image-form" onchange="axios_submit(this);" multiple>
 		</form>
+	</div>
 	@endcan
+	<div class="">
+		<span>右翻模特图</span>
+	</div>
 </div>
