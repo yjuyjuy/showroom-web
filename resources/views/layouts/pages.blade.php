bc7976f5f23c1e951c957b01855b56cd38cf6bb2
Chris <yjuyjuy@gmail.com>
2020-01-09 16:10:53 +0800

    fix bug

--- resources/views/layouts/pages.blade.php
+++ resources/views/layouts/pages.blade.php
@@ -1,12 +1,5 @@
 <div class="d-flex w-100 justify-content-center">
-	<?php $route_params = [
-			'vendor' => $vendor ?? null,
-			'retailer' => $retailer ?? null,
-			'designer' => $designer ?? null,
-			'category' => $category ?? null,
-			'brand' => $brand ?? null,
-			'department' => $department ?? null,
-		]; ?>
+	<?php $route_params = Route::current()->parameters(); ?>
 		@if($page > 1)
 		<a href="{{ route(Route::currentRouteName(), array_merge($route_params, request()->query(), ['page' => $page - 1,])) }}" class="mdc-button">
 			<span class="mdc-button__label">上一页</span>
