d3c1ee87cd304421cee3de91fee2eca4a7346e0f
Chris <yjuyjuy@gmail.com>
2020-01-26 16:32:07 +0800

    testing api for mobile app

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,25 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
 	return $request->user();
 });
+
+Route::get('/products', function() {
+	$products = \App\Product::orderBy('updated_at', 'desc')->get();
+	$total_pages = ceil($products->count() / 48.0);
+	$page = min(max(request()->query('page', 1), 1), $total_pages);
+	$products = $products->forPage($page, 48);
+	$products->load(['brand', 'images']);
+	return [
+		'page' => $page,
+		'total_pages' => $total_pages,
+		'user' => auth()->user(),
+		'products' => $products->values(),
+	];
+});
+Route::get('/products/{product}', function(\App\Product $product) {
+	return $product->load([
+		'brand', 'season', 'color', 'category', 'prices', 'prices.vendor',
+		'images' => function($query) {
+			$query->orderBy('order', 'asc');
+		},
+	]);
+});
