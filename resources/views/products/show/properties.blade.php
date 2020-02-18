3c40f7b9390509fdc1c4ae0bcecb1c53eb956376
Chris <yjuyjuy@gmail.com>
2020-02-18 14:23:37 +0800

    change text 更多详情 => 官网页面

--- resources/views/products/show/properties.blade.php
+++ resources/views/products/show/properties.blade.php
@@ -30,7 +30,7 @@
 	<div class="d-flex justify-content-end">
 		<div class="mdc-menu-surface--anchor">
 			<button type="button" class="mdc-button open-menu-button">
-				<span class="mdc-button__label">更多详情</span>
+				<span class="mdc-button__label">官网页面</span>
 				<i class="material-icons mdc-button__icon" aria-hidden="true">arrow_drop_down</i>
 			</button>
 			<div class="mdc-menu mdc-menu-surface mdc-menu--with-button">
