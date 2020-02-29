98f63efd88778b89a9f557d386f962813eb5b208
Chris <yjuyjuy@gmail.com>
2020-02-29 14:28:33 +0800

    update routes for api testing

--- routes/api.php
+++ routes/api.php
@@ -18,21 +18,23 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/products', function() {
-	$products = \App\Product::orderBy('updated_at', 'desc')->get();
-	$total_pages = ceil($products->count() / 48.0);
-	$page = min(max(request()->query('page', 1), 1), $total_pages);
-	$products = $products->forPage($page, 48);
-	$products->load(['brand', 'images']);
-	return [
-		'page' => $page,
-		'total_pages' => $total_pages,
-		'user' => auth()->user(),
-		'products' => $products->values(),
-	];
+	return \Illuminate\Support\Facades\Cache::remember('api-products-page'.request()->query('page', 1), 5 * 60, function() {
+		$products = \App\Product::orderBy('updated_at', 'desc')->get();
+		$total_pages = ceil($products->count() / 24.0);
+		$page = min(max(request()->query('page', 1), 1), $total_pages);
+		$products = $products->forPage($page, 24);
+		$products->load(['brand', 'images', 'season', 'retails', 'retails.retailer', 'offers', 'offers.vendor']);
+		return [
+			'page' => $page,
+			'total_pages' => $total_pages,
+			'user' => auth()->user(),
+			'products' => $products->values(),
+		];
+	});
 });
 Route::get('/products/{product}', function(\App\Product $product) {
 	return $product->load([
-		'brand', 'season', 'color', 'category', 'prices', 'prices.vendor',
+		'brand', 'season', 'color', 'category', 'prices', 'prices.vendor', 'retails', 'retails.retailer', 'offers', 'offers.vendor',
 		'images' => function($query) {
 			$query->orderBy('order', 'asc');
 		},
