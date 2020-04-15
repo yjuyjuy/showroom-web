f8e95daf8b7b06ccaea5dda6547567749901435b
Chris <yjuyjuy@gmail.com>
2020-04-15 16:18:17 -0500

    refactor route paths

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -13,31 +13,31 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::post('/register', 'api\v1\RegisterController@handle');
+Route::post('register', 'api\v1\RegisterController@handle');
 
 Route::middleware(['auth:api', 'vendor'])->group(function() {
-	Route::get('/user', function() {
+	Route::get('user', function() {
 		return [
 			'user' => auth()->user(),
 			'token' => auth()->user()->token(),
 		];
 	});
 
-	Route::get('/products', 'api\v1\ProductController@index');
-	Route::get('/products/{product}', 'api\v1\ProductController@show');
-	Route::post('/products/{product}/follow', 'ProductController@follow');
-	Route::post('/products/{product}/unfollow', 'ProductController@unfollow');
+	Route::get('products', 'api\v1\ProductController@index');
+	Route::get('products/{product}', 'api\v1\ProductController@show');
+	Route::post('products/{product}/follow', 'ProductController@follow');
+	Route::post('products/{product}/unfollow', 'ProductController@unfollow');
 
-	Route::get('/prices', 'api\v1\PriceController@index');
-	Route::post('/prices', 'api\v1\PriceController@store');
-	Route::patch('/prices/{price}', 'api\v1\PriceController@update');
-	Route::delete('/prices/{price}', 'api\v1\PriceController@destroy');
+	Route::get('prices', 'api\v1\PriceController@index');
+	Route::post('prices', 'api\v1\PriceController@store');
+	Route::patch('prices/{price}', 'api\v1\PriceController@update');
+	Route::delete('prices/{price}', 'api\v1\PriceController@destroy');
 	Route::post('prices/{price}/+/{size}', 'PriceController@add');
 	Route::post('prices/{price}/-/{size}', 'PriceController@subtract');
 
-	Route::post('/products/{product}/measurement', 'MeasurementController@store');
-	Route::patch('/products/{product}/measurement', 'MeasurementController@update');
-	Route::delete('/products/{product}/measurement', 'MeasurementController@destroy');
+	Route::post('products/{product}/measurement', 'MeasurementController@store');
+	Route::patch('products/{product}/measurement', 'MeasurementController@update');
+	Route::delete('products/{product}/measurement', 'MeasurementController@destroy');
 
-	Route::get('/vendors', 'api\v1\VendorController@index');
+	Route::get('vendors', 'api\v1\VendorController@index');
 });
