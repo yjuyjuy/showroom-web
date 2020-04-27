cb37e1f6eb4d50ce29e154c8cf9c58b1c9bff6c2
Chris <yjuyjuy@gmail.com>
2020-04-27 18:08:48 -0500

    copy RegisterController from v2/customer

--- app/Http/Controllers/api/v1/RegisterController.php
+++ app/Http/Controllers/api/v1/RegisterController.php
@@ -2,37 +2,88 @@
 
 namespace App\Http\Controllers\api\v1;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\User;
-use App\InviteCode;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    public function handle(Request $request)
-		{
-			$data = $request->validate([
-				'username' => ['required', 'string', 'max:255', 'unique:users'],
-				'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-				'password' => ['required', 'string', 'min:8', 'confirmed'],
-				'invite_code' => ['nullable', 'string'],
-			]);
-			do {
-				$id = random_int(1000000000, 9999999999);
-			} while(User::find($id));
-			$type = NULL;
-			if ($data['invite_code']) {
-				$code = InviteCode::find($data['invite_code']);
-				if ($code) {
-					$type = 'invited:'.$code->vendor->id;
-				}
-			}
-			return User::create([
-				'id' => $id,
-				'email' => $data['email'],
-				'username' => $data['username'],
-				'password' => Hash::make($data['password']),
-				'type' => $type,
-			]);
-		}
+	/*
+	|--------------------------------------------------------------------------
+	| Register Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles the registration of new users as well as their
+	| validation and creation. By default this controller uses a trait to
+	| provide this functionality without requiring any additional code.
+	|
+	*/
+
+	use RegistersUsers;
+
+	/**
+	 * Where to redirect users after registration.
+	 *
+	 * @var string
+	 */
+	protected $redirectTo = '/api/v1/user';
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('guest');
+	}
+
+	/**
+	 * Get a validator for an incoming registration request.
+	 *
+	 * @param  array  $data
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	protected function validator(array $data)
+	{
+		return Validator::make($data, [
+			'username' => ['required', 'string', 'max:255', 'unique:users'],
+			'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+			'password' => ['required', 'string', 'min:8', 'confirmed'],
+		]);
+	}
+
+	/**
+	 * Create a new user instance after a valid registration.
+	 *
+	 * @param  array  $data
+	 * @return \App\User
+	 */
+	protected function create(array $data)
+	{
+		do {
+			$id = random_int(1000000000, 9999999999);
+		} while(\App\User::find($id));
+		return User::create([
+			'id' => $id,
+			'email' => $data['email'],
+			'username' => $data['username'],
+			'password' => Hash::make($data['password']),
+		]);
+	}
+
+	/**
+	 * The user has been registered.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  mixed  $user
+	 * @return mixed
+	 */
+	protected function registered(Request $request, $user)
+	{
+		return ['success'];
+	}
 }
