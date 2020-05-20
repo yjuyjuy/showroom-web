b29a993f387caf2d32121b498cf14856b5630717
Chris <yjuyjuy@gmail.com>
2020-05-20 17:43:41 -0500

    make Order model

---
+++ app/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+	protected $guarded = [];
+	public $incrementing = false;
+	protected $keyType = 'string';
+
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
+
+	public function vendor()
+	{
+		return $this->belongsTo(Vendor::class);
+	}
+
+	public function product()
+	{
+		return $this->belonsTo(Product::class);
+	}
+}
