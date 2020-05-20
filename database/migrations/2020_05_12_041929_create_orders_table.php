7f54ac6d075a245fd7ea23753acf7def7b78a50e
Chris <yjuyjuy@gmail.com>
2020-05-20 18:26:12 -0500

    fix bug

--- database/migrations/2020_05_12_041929_create_orders_table.php
+++ database/migrations/2020_05_12_041929_create_orders_table.php
@@ -6,46 +6,46 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateOrdersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('orders', function (Blueprint $table) {
-            $table->string('id');
-            $table->unsignedBigInteger('user_id');
-						$table->unsignedBigInteger('product_id');
-            $table->unsignedBigInteger('vendor_id');
-            $table->string('size');
-            $table->string('name');
-            $table->string('phone');
-            $table->string('address1');
-            $table->string('address2');
-            $table->string('city');
-            $table->string('state');
-            $table->string('country');
-						$table->string('zip');
-            $table->boolean('is_direct');
-						$table->enum('status', ['created', 'confirmed', 'paid', 'shipped', 'delivered', 'completed', 'closed']);
-						$table->timestamp('confirmed_at');
-						$table->timestamp('paid_at');
-						$table->timestamp('shipped_at');
-						$table->timestamp('delivered_at');
-						$table->timestamp('completed_at');
-						$table->timestamp('closed_at');
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('orders', function (Blueprint $table) {
+			$table->string('id');
+			$table->unsignedBigInteger('user_id');
+			$table->unsignedBigInteger('product_id');
+			$table->unsignedBigInteger('vendor_id');
+			$table->string('size');
+			$table->string('name');
+			$table->string('phone');
+			$table->string('address1');
+			$table->string('address2');
+			$table->string('city');
+			$table->string('state');
+			$table->string('country');
+			$table->string('zip');
+			$table->boolean('is_direct');
+			$table->enum('status', ['created', 'confirmed', 'paid', 'shipped', 'delivered', 'completed', 'closed']);
+			$table->timestamp('confirmed_at')->nullable();
+			$table->timestamp('paid_at')->nullable();
+			$table->timestamp('shipped_at')->nullable();
+			$table->timestamp('delivered_at')->nullable();
+			$table->timestamp('completed_at')->nullable();
+			$table->timestamp('closed_at')->nullable();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('orders');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('orders');
+	}
 }
