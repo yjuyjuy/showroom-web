56d50121abbbf45c279b72b305e64a53a84a6cfd
Chris <yjuyjuy@gmail.com>
2020-05-20 19:17:21 -0500

    fix bug

--- app/Http/Controllers/api/v1/OrderController.php
+++ app/Http/Controllers/api/v1/OrderController.php
@@ -62,9 +62,7 @@ class OrderController extends Controller
 			'zip' => $address->zip,
 			'status' => 'created',
 		], $data);
-		return Order::create($data)->load([
-			'product', 'product.images', 'product.season', 'product.color', 'product.brand', 'vendor',
-		]);
+		return Order::create($data);
 	}
 
 	/**
