2b81454a1759183925386c2ef0f71e2fd6195c1a
Chris <yjuyjuy@gmail.com>
2020-05-21 22:19:17 -0500

    cast is_direct field as boolean value

--- app/Order.php
+++ app/Order.php
@@ -9,6 +9,14 @@ class Order extends Model
 	protected $guarded = [];
 	public $incrementing = false;
 	protected $keyType = 'string';
+	/**
+	 * The attributes that should be cast.
+	 *
+	 * @var array
+	 */
+	protected $casts = [
+		'is_direct' => 'boolean',
+	];
 
 	public function user()
 	{
