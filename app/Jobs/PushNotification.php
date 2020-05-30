ebf37c6ba1ab57752302754dd39cb861a57f1028
Chris <yjuyjuy@gmail.com>
2020-05-30 15:04:09 -0500

    make PushNotification job

---
+++ app/Jobs/PushNotification.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+
+class PushNotification implements ShouldQueue
+{
+	use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+	protected $token;
+	protected $title;
+	protected $body;
+
+	/**
+	 * Create a new job instance.
+	 *
+	 * @return void
+	 */
+	public function __construct($token, $title = null, $body = null)
+	{
+		$this->token = $token;
+		$this->title = $title;
+		$this->body = $body;
+	}
+
+	/**
+	 * Execute the job.
+	 *
+	 * @return void
+	 */
+	public function handle()
+	{
+		$response = Http::withHeaders([
+			'Content-Type' => 'application/json',
+			'Authorization' => 'key='.env('FCM_SERVER_KEY'),
+		])->post('https://fcm.googleapis.com/fcm/send', [
+			'to'=> $this->token,
+			'notification' => [
+				'title' => $this->title,
+				'body' => $this->body,
+			],
+			'priority'=>'high',
+		]);
+	}
+}
