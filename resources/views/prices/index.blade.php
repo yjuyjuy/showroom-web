197ec14fad08efaae6b26db3c158e7b98e605ab7
Chris <yjuyjuy@gmail.com>
2020-05-29 22:50:04 -0500

    only show not empty brands

--- resources/views/prices/index.blade.php
+++ resources/views/prices/index.blade.php
@@ -11,7 +11,7 @@
 	  <div class="mdc-select__selected-text"></div>
 	  <div class="mdc-select__menu mdc-menu mdc-menu-surface">
 	    <ul class="mdc-list">
-			@foreach(\App\Brand::all() as $brand)
+			@foreach($products->pluck('brand')->unique() as $brand)
 	      		<li class="mdc-list-item" data-value="{{ $brand->id }}">{{ $brand->name }}</li>
 			@endforeach
 	    </ul>
