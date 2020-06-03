a3ddcf98d0b23c9069cfbe79e5910749da280e90
Chris <yjuyjuy@gmail.com>
2020-06-02 21:55:24 -0500

    make api\v3\seller\MeasurementController

---
+++ app/Http/Controllers/api/v3/seller/MeasurementController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\api\v3;
+
+use Illuminate\Http\Request;
+use App\Product;
+use App\Measurement;
+
+class MeasurementController extends Controller
+{
+	public function store(Request $request, Product $product)
+	{
+		(new \App\Http\Controllers\MeasurementController())->store($request, $product);
+		return ['measurement' => $product->measurement->fresh()];
+	}
+
+	public function update(Request $request, Product $product)
+	{
+		(new \App\Http\Controllers\MeasurementController())->update($request, $product);
+		return ['measurement' => $product->measurement->fresh()];
+	}
+
+	public function destroy(Request $request, Product $product)
+	{		
+		(new \App\Http\Controllers\MeasurementController())->destroy($request, $product);
+		return ['success'];
+	}
+}
