d5a3a7aeafac86c8b6fd99772daef30339298a26
Chris <yjuyjuy@gmail.com>
2020-06-04 17:06:33 -0500

    fix bug

--- app/Order.php
+++ app/Order.php
@@ -17,6 +17,19 @@ class Order extends Model
 	protected $casts = [
 		'is_direct' => 'boolean',
 	];
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+		'confirmed_at',
+		'paid_at',
+		'shipped_at',
+		'delivered_at',
+		'completed_at',
+		'closed_at',
+    ];
 
 	public function user()
 	{
