8147c7e7e9b063c821cfcb253631865579352b72
ITX2018 <yjuyjuy@gmail.com>
2020-06-25 22:16:06 -0500

    share the same AddressController for seller and customer api

--- app/Http/Controllers/api/v3/seller/AddressController.php
+++ app/Http/Controllers/api/v3/seller/AddressController.php
@@ -2,69 +2,10 @@
 
 namespace App\Http\Controllers\api\v3\seller;
 
-use App\Address;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\api\v3\shared\AddressController as Controller;
 
 class AddressController extends Controller
 {
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store(Request $request)
-	{
-		$data = $this->validateRequest();
-		$data['user_id'] = auth()->user()->id;
-		return Address::create($data);
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \App\Address  $address
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update(Request $request, Address $address)
-	{
-		$address->update($this->validateRequest());
-		return $address->fresh();
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  \App\Address  $address
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy(Address $address)
-	{
-		return ['success' => $address->delete()];
-	}
-
-	public function validateRequest()
-	{
-		$data = request()->validate([
-			'name' => 'nullable|string',
-			'phone' => 'nullable|string',
-			'address1' => 'nullable|string',
-			'address2' => 'nullable|string',
-			'city' => 'nullable|string',
-			'state' => 'nullable|string',
-			'country' => 'nullable|string',
-			'zip' => 'nullable|string',
-		]);
-		$data['name'] = $data['name'] ?? '';
-		$data['phone'] = $data['phone'] ?? '';
-		$data['address1'] = $data['address1'] ?? '';
-		$data['address2'] = $data['address2'] ?? '';
-		$data['city'] = $data['city'] ?? '';
-		$data['state'] = $data['state'] ?? '';
-		$data['country'] = $data['country'] ?? '';
-		$data['zip'] = $data['zip'] ?? '';
-		return $data;
-	}
+	//
 }
