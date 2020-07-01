e8fd1d17f41c8a745039ff5960f2a105731258eb
yjuyjuy@itx2018 <yjuyjuy@gmail.com>
2020-07-01 10:45:43 -0500

    add fromUser helper attribute

--- app/Message.php
+++ app/Message.php
@@ -14,4 +14,17 @@ class Message extends Model
     {
         return $this->morphTo();
     }
+    public function fromUser(User $user)
+    {
+        $accounts = $user->accounts;
+        foreach ($accounts as $account) {
+            if (
+                $this->sender_id == $account->id
+                && $this->sender_type == $account::class
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
