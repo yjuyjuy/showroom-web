cbf77b63e4fa78bda2856a6a0c74e0893cf3030b
yjuyjuy@itx2018 <yjuyjuy@gmail.com>
2020-07-06 00:57:20 -0500

    load missing relationships

--- app/Http/Controllers/api/v3/seller/UserController.php
+++ app/Http/Controllers/api/v3/seller/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
 {
 	public function show()
 	{
-		$user = auth()->user()->load(['image', 'addresses', 'following_vendors', 'vendor', 'vendor.image', 'vendor.retailer', 'vendor.retailer.image']);
+		$user = auth()->user()->load(['image', 'addresses', 'following_vendors', 'following_vendors.image', 'vendor', 'vendor.image', 'vendor.retailer', 'vendor.retailer.image']);
 		$user->following_products = $user->following_products()->pluck('product_id');
 		return [
 			'user' => $user,
