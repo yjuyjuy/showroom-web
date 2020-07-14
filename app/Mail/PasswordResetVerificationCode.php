4f8ab00f6f808e3e571c210c4fc1d10e63e56710
yjuyjuy@itx2018 <yjuyjuy@gmail.com>
2020-07-14 00:59:02 -0500

    make mailable for password reset

---
+++ app/Mail/PasswordResetVerificationCode.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PasswordResetVerificationCode extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $token;
+    public $user;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($user, $token)
+    {
+        $this->user = $user;
+        $this->token = $token;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Showroom验证码')->view('emails.forgot_password');
+    }
+}
