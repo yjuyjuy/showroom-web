205981179d9b8dae896f9b911e5970c1b790418b
yjuyjuy@itx2018 <yjuyjuy@gmail.com>
2020-07-14 21:39:17 -0500

    fix bug and refactor code

--- app/Http/Controllers/api/v3/shared/AuthController.php
+++ app/Http/Controllers/api/v3/shared/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api\v3\shared;
 use App\User;
 use App\Http\Controllers\Controller;
 use App\Mail\PasswordResetVerificationCode;
+use Carbon\Carbon;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -17,10 +18,7 @@ class AuthController extends Controller
 
     public function forgot(Request $request)
     {
-        $data = $request->validate([
-            'email' => 'required|email|exists:users,email',
-        ]);
-        $user = User::firstWhere('email', $data['email']);
+        $user = $this->validateUser($request);
         $token = random_int(10 ** 4, 10 ** 5 - 1);
         DB::table($this->table)->insert(['email' => $user->email, 'token' => $token]);
         Mail::to($user)->send(new PasswordResetVerificationCode($user, $token));
@@ -29,41 +27,52 @@ class AuthController extends Controller
 
     public function verify(Request $request)
     {
-        $data = $request->validate([
-            'email' => 'required|email|exists:users,email',
-            'token' => 'required|integer',
-        ]);
-        $row = DB::table($this->table)
-            ->where('email', $data['email'])
-            ->where('token', (int) $data['token'])
-            ->whereRaw('created_at > current_timestamp - interval 5 minute')
-            ->first();
-        if ($row) {
-            return null;
-        } else {
-            abort(403);
-        }
+        $this->validateUser($request);
+        $this->validateToken($request);
+        return null;
     }
 
     public function reset(Request $request)
     {
+        $user = $this->validateUser($request);
+        $this->validateToken($request);
         $data = $request->validate([
-            'email' => 'required|email|exists:users,email',
-            'token' => 'required|integer',
             'password' => 'required|string|min:8|confirmed',
+        ]);
+        $user->password = Hash::make($data['password']);
+        $user->save();
+        return null;
+    }
+
+    public function validateUser(Request $request)
+    {
+        $email = $request->validate([
+            'email' => 'required|email',
+        ])['email'];
+        $user = User::firstWhere('email', $email);
+        if ($user) {
+            return $user;
+        } else {
+            abort(403, 'User not found');
+        }
+    }
+
+    public function validateToken(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'required|email',
+            'token' => 'required|integer',
         ]);
         $row = DB::table($this->table)
             ->where('email', $data['email'])
-            ->where('token', (int) $data['token'])
-            ->whereRaw('created_at > current_timestamp - interval 5 minute')
+            ->where('token', $data['token'])
             ->first();
-        if ($row) {
-            $user = User::firstWhere('email', $data['email']);
-            $user->password = Hash::make($data['password']);
-            $user->save();
-            return '';
-        } else {
-            abort(403);
+        if (!$row) {
+            abort(403, 'Token not found');
+        }
+        $created_at = new Carbon($row->created_at);
+        if (now()->isBefore($created_at->addMinutes(5))) {
+            abort(403, 'Token expired');
         }
     }
 }
