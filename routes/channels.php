654b73d53f2151a27ce963848acb47d18f6f3bf0
yjuyjuy@itx2018 <yjuyjuy@gmail.com>
2020-07-19 14:08:09 -0500

    define broadcast channel authorization callbacks

--- routes/channels.php
+++ routes/channels.php
@@ -11,6 +11,25 @@
 |
 */
 
+use App\Order;
+
+
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('user.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
+Broadcast::channel('vendor.{id}', function ($user, $id) {
+    return (int) $user->vendor_id === (int) $id;
+});
+Broadcast::channel('retailer.{id}', function ($user, $id) {
+    return (int) $user->vendor->retailer_id === (int) $id;
+});
+Broadcast::channel('order.{order}', function ($user, Order $order) {
+    if (!$order) return false;
+    return $user->id === $order->user_id || ($user->vendor &&
+        ($order->seller->is($user->vendor) ||
+            ($user->vendor->retailer && $order->seller->is($user->vendor->retailer))));
 });
