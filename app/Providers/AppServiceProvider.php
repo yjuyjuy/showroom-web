8f37a13bea7c388f57d918eccd9b56d5bd8f2443
yjuyjuy@itx2018 <yjuyjuy@gmail.com>
2020-07-20 20:20:31 -0500

    replace laravel-echo-server with laravel-websocket package

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
 	{
 		\App\VendorPrice::observe(\App\Observers\PriceObserver::class);
 		\Illuminate\Support\Facades\URL::forceScheme('https');
+
+		Gate::define('viewWebSocketsDashboard', function ($user = null) {
+			return $user && $user->is_admin;
+		});
 	}
 }
