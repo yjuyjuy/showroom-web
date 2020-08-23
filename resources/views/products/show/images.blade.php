af5104cbc876c0bd273fb4a4db2fbb867659005e
yjuyjuy@itx2018 <yjuyjuy@gmail.com>
2020-08-23 15:45:00 -0500

    clean up unused text

--- resources/views/products/show/images.blade.php
+++ resources/views/products/show/images.blade.php
@@ -1,23 +1,9 @@
 
 <carousel :images='@json($product->images->pluck('url')->toArray())'></carousel>
-<div class="d-flex justify-content-between">
-	<div class="">
-		<span>左翻街拍图</span>
-	</div>
+<div class="d-flex justify-content-center">
 	@if(auth()->user() && auth()->user()->is_admin)
 	<div class="">
 		<a href="{{route('images.edit',['product'=>$product])}}">管理图片</a>
 	</div>
-	<div class="">
-		<a href="#" onclick="event.preventDefault(); document.getElementById('images-input').click();">上传图片</a>
-		<form id="upload-image-form" class="" action="{{ route('images.store') }}" method="post" style="display:none;">
-			@csrf
-			<input type="text" name="product_id" value="{{ $product->id }}">
-			<input id="images-input" type="file" name="images" form="upload-image-form" onchange="axios_submit(this);" multiple>
-		</form>
-	</div>
 	@endif
-	<div class="">
-		<span>右翻模特图</span>
-	</div>
 </div>
