cd133c1613a5537505b1e32bf437e962773c1c7f
yjuyjuy@itx2018 <yjuyjuy@gmail.com>
2020-08-23 20:54:15 -0500

    refactor code

--- app/Jobs/OptimizeImage.php
+++ app/Jobs/OptimizeImage.php
@@ -11,7 +11,9 @@ use Illuminate\Foundation\Bus\Dispatchable;
 class OptimizeImage implements ShouldQueue
 {
 	use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-
+	
+	const DEFAULT_RATIO = 1.413;
+	
 	protected $path;
 
 	/**
@@ -31,63 +33,67 @@ class OptimizeImage implements ShouldQueue
 	 */
 	public function handle()
 	{
+
 		// $size_limit1 = 20 * 1024;
 		// $size_limit2 = 50 * 1024;
 
-		$path = public_path('storage/'.$this->path);
+		$path = public_path('storage/' . $this->path);
 		$image = \Intervention\Image\Facades\Image::make($path);
 		$w = $image->width();
 		$h = $image->height();
 		// too wide
 		if ($h / $w < 1.35) {
-			$diff = $this->diff($w, $h, $image);
+			$diff = $this->diff($w, $h, $image, self::DEFAULT_RATIO);
 			if ($diff >= 100) {
 				$isCropped = false;
+				// width > height
 				if ($h / $w < 1.0) {
 					$diff = $this->diff($w, $h, $image, 1.0);
+					// crop to square then upsize
 					if ($diff < 100) {
-						\Intervention\Image\Facades\Image::canvas($h, $h * 1.413, '#ffffff')->insert($image->crop($h, $h), 'center')->save($path.'_upsized.jpeg', 100, 'jpeg');
-						$path = $path.'_upsized.jpeg';
+						$image->crop($h, $h)->resizeCanvas($h, $h * self::DEFAULT_RATIO, null, false, '#ffffff')->save($path . '_upsized.jpeg', 100, 'jpeg');
+						$path = $path . '_upsized.jpeg';
 						$isCropped = true;
 					}
 				}
+				// upsize
 				if (!$isCropped) {
-					\Intervention\Image\Facades\Image::canvas($w, $w * 1.413, '#ffffff')->insert($image, 'center')->save($path.'_upsized.jpeg', 100, 'jpeg');
-					$path = $path.'_upsized.jpeg';
+					$image->resizeCanvas($w, $w * self::DEFAULT_RATIO, null, false, '#ffffff')->save($path . '_upsized.jpeg', 100, 'jpeg');
+					$path = $path . '_upsized.jpeg';
 				}
 			} else {
-				\Intervention\Image\Facades\Image::canvas($w, $w * 1.413, '#ffffff')->insert($image, 'center')->save($path.'_upsized.jpeg', 100, 'jpeg');
+				$image->resizeCanvas($w, $w * self::DEFAULT_RATIO, null, false, '#ffffff')->save($path . '_upsized.jpeg', 100, 'jpeg');
 			}
 			// too tall
-		} elseif ($h / $w > 1.413) {
-			$diff = $this->diff($w, $h, $image);
+		} elseif ($h / $w > self::DEFAULT_RATIO) {
+			$diff = $this->diff($w, $h, $image, self::DEFAULT_RATIO);
 			if ($diff >= 100) {
-				\Intervention\Image\Facades\Image::canvas($h / 1.413, $h, '#ffffff')->insert($image, 'center')->save($path.'_upsized.jpeg', 100, 'jpeg');
-				$path = $path.'_upsized.jpeg';
+				$image->resizeCanvas($h / self::DEFAULT_RATIO, $h, null, false, '#ffffff')->save($path . '_upsized.jpeg', 100, 'jpeg');
+				$path = $path . '_upsized.jpeg';
 			}
 		}
 		// $quality = 100;
 		// do {
 		// $quality = $quality - 10;
-		$image = \Intervention\Image\Facades\Image::make($path)->fit(400, 565)->save(public_path('storage/'.$this->path).'_400.jpeg', 50);
+		$image = \Intervention\Image\Facades\Image::make($path)->fit(400, 565)->save(public_path('storage/' . $this->path) . '_400.jpeg', 50);
 		// } while ($image->fileSize() > $size_limit1 && $quality > 30);
 
 		// $quality = 100;
 		// $image = \Intervention\Image\Facades\Image::make($path);
 		// $width = min(800, $w);
 		// $height = min(1130, $h);
-		// if ($width * 1.413 > $height) {
-		// 	$width = ceil($height / 1.413);
+		// if ($width * self::DEFAULT_RATIO > $height) {
+		// 	$width = ceil($height / self::DEFAULT_RATIO);
 		// } else {
-		// 	$height = ceil($width * 1.413);
+		// 	$height = ceil($width * self::DEFAULT_RATIO);
 		// }
 		// do {
 		// $quality = $quality - 10;
-		$image = \Intervention\Image\Facades\Image::make($path)->fit(800, 1130)->save(public_path('storage/'.$this->path).'_800.jpeg', 80);
+		$image = \Intervention\Image\Facades\Image::make($path)->fit(800, 1130)->save(public_path('storage/' . $this->path) . '_800.jpeg', 80);
 		// } while ($image->fileSize() > $size_limit2 && $quality > 30);
 	}
 
-	public function diff($w, $h, $image, $ratio = 1.413)
+	public function diff($w, $h, $image, $ratio)
 	{
 		if ($h / $w < $ratio) {
 			$x1 = ceil(($w - $h / $ratio) / 2);
