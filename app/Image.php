57387c0788a9acee7970cf39439e035335999334
yjuyjuy@itx2018 <yjuyjuy@gmail.com>
2020-08-23 21:12:05 -0500

    update image urls

--- app/Image.php
+++ app/Image.php
@@ -30,18 +30,18 @@ class Image extends Model
 	}
 	public function getSmallAttribute()
 	{
-		return secure_asset('storage/'.$this->path.'_400.jpeg');
+		return secure_asset(str_replace('images/', 'images/w200/', 'storage/' . $this->path, 1));
 	}
 	public function getMediumAttribute()
 	{
-		return secure_asset('storage/'.$this->path.'_800.jpeg');
+		return secure_asset(str_replace('images/', 'images/w400/', 'storage/' . $this->path, 1));
 	}
 	public function getLargeAttribute()
 	{
-		return secure_asset('storage/'.$this->path);
+		return secure_asset(str_replace('images/', 'images/w800/', 'storage/' . $this->path, 1));
 	}
 	public function getUrlAttribute()
 	{
-		return secure_asset('storage/'.$this->path.'_800.jpeg');
+		return secure_asset(str_replace('images/', 'images/w400/', 'storage/' . $this->path, 1));
 	}
 }
