8459db0de31dc81596cbe0aa6c182439f6b3b30e
yjuyjuy@itx2018 <yjuyjuy@gmail.com>
2020-10-20 21:36:17 -0500

    delete unused code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="dns-prefetch" href="https://cdn.notdopebxtch.com/">
 		<link href="/css/fonts.css" rel="stylesheet">
 		<!-- CSRF Token -->
 		<meta name="csrf-token" content="{{ csrf_token() }}">
