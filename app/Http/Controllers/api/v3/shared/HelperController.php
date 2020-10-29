4324301d17fe6eaa35e263fa5f7219efa26fb7f3
yjuyjuy@itx2018 <yjuyjuy@gmail.com>
2020-10-29 18:41:27 -0500

    add server method

--- app/Http/Controllers/api/v3/shared/HelperController.php
+++ app/Http/Controllers/api/v3/shared/HelperController.php
@@ -9,13 +9,28 @@ use Illuminate\Support\Facades\Http;
 class HelperController extends Controller
 {
     const GEO_API = 'https://api.ipgeolocationapi.com/geolocate';
+    const SERVERS = [
+        'CN' => 'https://cn.notdopebxtch.com',
+        'US' => 'https://cn.notdopebxtch.com',
+        'default' => 'https://us.notdopebxtch.com',
+    ];
     public function ip(Request $request)
     {
-        return ['ip' => $request->header('x-forwarded-for') ?? $request->header('remote_addr')];
+        return $request->header('x-forwarded-for') ?? $request->header('remote_addr');
     }
 
     public function geolocate(Request $request)
     {
-        return Http::get(self::GEO_API . "/{($request->header('x-forwarded-for') ?? $request->header('remote_addr'))}")->json();
+        return Http::get(self::GEO_API . "/{$this->ip($request)}")->json();
+    }
+
+    public function server(Request $request)
+    {
+        $countryCode = $this->geolocate($request)['alpha2'];
+        if (!empty($countryCode) && array_key_exists($countryCode, self::SERVERS)) {
+            return self::SERVERS[$countryCode];
+        } else {
+            return self::SERVERS['default'];
+        }
     }
 }
