57c86ce0258583dbcd7794a663bf84d38f8df959
yjuyjuy@itx2018 <yjuyjuy@gmail.com>
2020-10-29 18:31:40 -0500

    fix bug

--- app/Http/Controllers/api/v3/shared/HelperController.php
+++ app/Http/Controllers/api/v3/shared/HelperController.php
@@ -9,11 +9,13 @@ use Illuminate\Support\Facades\Http;
 class HelperController extends Controller
 {
     const GEO_API = 'https://api.ipgeolocationapi.com/geolocate';
-    public function ip(Request $request) {
-        return ['ip' => $request->header('x-forwarded-for')];
+    public function ip(Request $request)
+    {
+        return ['ip' => $request->header('x-forwarded-for') ?? $request->header('remote_addr')];
     }
 
-    public function geolocate(Request $request) {
-        return Http::get(self::GEO_API."/{$request->header('x-forwarded-for')}")->json();
+    public function geolocate(Request $request)
+    {
+        return Http::get(self::GEO_API . "/{($request->header('x-forwarded-for') ?? $request->header('remote_addr'))}")->json();
     }
 }
